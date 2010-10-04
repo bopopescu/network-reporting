9a4bdf22c0ae53023dda4a4838fb6a7de1c0fbec
Nafis Jamal <njamal@Macintosh.local>
2010-10-04 15:22:37 -0700

    handle "clear" response

--- server/publisher/views.py
+++ server/publisher/views.py
@@ -262,8 +262,8 @@ class ShowAppHandler(RequestHandler):
     
 
     # do a bar graph showing contribution of each site to impression count
+    pie_chart_urls = {}
     if len(a.sites) > 0:
-      pie_chart_urls = {}
       impressions_by_site = []
       clicks_by_site = []
       users_by_site = []
@@ -344,8 +344,9 @@ class ShowHandler(RequestHandler):
     yesterday = site.all_stats[-2]
     
     # do a bar graph showing contribution of each site to impression count
+    pie_chart_urls = {}
+    
     if len(site.adgroups) > 0:
-      pie_chart_urls = {}
       impressions_by_ag = []
       clicks_by_ag = []
       users_by_ag = []

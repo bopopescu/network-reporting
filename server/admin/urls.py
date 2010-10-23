f79d4bc9a54ced015ddfdcfc1927be4f3617e2f7
James Payne <jpayne@mopub-inc.com>
2010-10-22 18:04:01 -0700

    admin dashboard

--- server/admin/urls.py
+++ server/admin/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('admin.views',
   url(r'^switch_user/$','admin_switch_user',name='admin_switch_user'),
+  url(r'^d/$', 'dashboard', name='dashboard')
 )
--- server/settings.py
+++ server/settings.py
@@ -133,21 +133,4 @@ INSTALLED_APPS = (
 
 IGNORE_APP_URLSAUTO = ('website')
 
-WHITELIST_EMAILS = (
-  'test@example.com',
-  'test2@example.com',
-  'lamen@user.com',
-  'nafisjamal@gmail.com',
-  'jim.e.payne@gmail.com',
-  'bryan.h.atwood@gmail.com',
-  'nafisjamal@mopub.com',
-  'batwood@mopub.com',
-  'jpayne@mopub.com',
-  'test2@example.com',
-  'batwood@mopub-inc.com',
-  'rudro@pixar.com',
-  'ra.roath@mac.com',
-  'akshay.kothari@gmail.com',
-  'jon@todoroo.com')
-
 from common.ragendja.settings_post import *

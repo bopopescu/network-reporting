7431f9469daf694d4eb4bd0061e641a8c79deae0
Sumul Shah <sumul@studioshah.com>
2011-01-14 17:42:18 -0800

    * More UI improvements and fixes * Custom date selection dialog (smart about min/max) * Charts!

--- js/mopub.js
+++ js/mopub.js
@@ -2,6 +2,9 @@
 	MoPub Global JS
 */
 
+// global mopub object
+var mopub = mopub || {};
+
 (function($){
 	// dom ready
 	$(document).ready(function() {
@@ -16,23 +19,17 @@
 		// set up buttonsets
 		$('.buttonset').buttonset();
 		
-		// set up dateOptions
-		$('.dateOptions input').click(function() {
-			var option = $(this).val();
-			if(option == 'custom') {
-				alert('Custom');
-			}
-			else {
-				// TODO: tell server about selected option to get new data
-			}
-		});
-		
 		// override default jQuery UI dialog options
 		$.extend($.ui.dialog.prototype.options, {
 			modal: true,
 			resizable: false,
 			draggable: false,
 			width: 400
+		});
+		
+		// override default jQuery UI datepicker options
+		$.datepicker.setDefaults({
+			dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 		});
 		
 		/*---------------------------------------/
@@ -81,6 +78,132 @@
 				var table = row.parents('table');
 				$('tr.active', table).removeClass('active');
 				row.addClass('active');
+			}
+		});
+		
+		/*---------------------------------------/
+		/ Highcharts default options
+		/---------------------------------------*/
+
+		Highcharts.setOptions({
+			chart: {
+				animation: false,
+				backgroundColor: null,
+				borderRadius: 0,
+				margin: [30,0,30,45]
+			},
+			title: { text: null },
+			lang: {
+				loading: "Loading ..."
+			}, 
+			credits: { enabled: false },
+			style: {
+				fontFamily: '"myriad-pro-1", "myriad-pro-2", Helvetica, Arial, sans-serif'
+			},
+			plotOptions: {
+				series: {
+					animation: false,
+					shadow: false,
+					stickyTracking: false
+				},
+				area: {
+					lineWidth: 4,
+					fillOpacity: 0.1,
+					stacking: 'normal',
+					marker: {
+						lineWidth: 2,
+						radius: 5,
+						symbol: 'circle',
+						states: {
+							hover: {
+								lineWidth: 2,
+								radius: 7
+							}
+						}
+					},
+					states: {
+						hover: {
+							lineWidth: 4
+						}
+					}
+				}
+			},
+			xAxis: {
+				endOnTick: false,
+				gridLineWidth: 0.5,
+				gridLineColor: '#dddddd',
+				lineWidth: 1,
+				lineColor: '#cccccc',
+				type: 'datetime',
+				labels: {
+					style: {
+						fontFamily: '"myriad-pro-1", "myriad-pro-2", Helvetica, Arial, sans-serif',
+						color: '#999',
+						fontSize: '10px'
+					},
+					y: 20
+				},
+				dateTimeLabelFormats: {
+					second: '%b %e %l:%M:%S%p',
+					minute: '%b %e %l:%M%p',
+					hour: '%b %e %l:%M%p',
+					day: '%b %e',
+					week: '%b %e',
+					month: '%b %Y',
+					year: '%Y'
+				},
+				tickColor: '#dddddd',
+				tickLength: 5,
+				tickWidth: 0.5
+			},
+			yAxis: {
+				showFirstLabel: false,
+				showLastLabel: true,
+				gridLineWidth: 0.5,
+				gridLineColor: '#dddddd',
+				min: 0,
+				title: {
+					text: null
+				},
+				labels: {
+					style: {
+						fontFamily: '"myriad-pro-1", "myriad-pro-2", Helvetica, Arial, sans-serif',
+						color: '#999',
+						fontSize: '10px'
+					},
+					x: -5
+				}
+			},
+			legend: {
+				borderColor: null,
+				borderRadius: 0,
+				borderWidth: 0,
+				align: 'center',
+				verticalAlign: 'top',
+				y: -17,
+				itemStyle: {
+					fontFamily: '"myriad-pro-1", "myriad-pro-2", Helvetica, Arial, sans-serif',
+					size: '12px',
+					cursor: 'pointer',
+					color: '#444444'
+				},
+				itemHoverStyle: {
+					color: '#e57300'
+				},
+				itemHiddenStyle: {
+					color: '#ccc'
+				},
+				lineHeight: 12,
+				symbolPadding: 6,
+				symbolWidth: 12
+			},
+			tooltip: {
+				backgroundColor: "rgba(255, 255, 255, .9)",
+				style: {
+					fontFamily: '"myriad-pro-1", "myriad-pro-2", Helvetica, Arial, sans-serif',
+					fontSize: '13px',
+					padding: '10px'
+				}
 			}
 		});
 		

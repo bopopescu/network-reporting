51adcb9f0d6d6f27a57cc68871a2eb568018e8fd
Jim <jpayne@alum.mit.edu>
2011-01-26 10:57:49 -0800

    dynamic front page

--- wp-theme/front-page.php
+++ wp-theme/front-page.php
@@ -1,52 +1,76 @@
 <?php get_header(); ?>
 
+
       <div id="titlebar">
         <div class="pageWidth">
-          <div id="home-intro">
-            <h1 id="home-headline">Ad Serving for Mobile Applications</h1>
-            <p>Run, measure, and optimize the ads in your mobile apps. Include <strong>all the major ad networks</strong> and your own custom content. Maximize revenue and engagement with <strong>powerful analytics</strong>. All on a <strong>flexible and open platform</strong> built by mobile app developers for mobile app developers.</p>
-            <div id="home-cta">
-              <span class="buttonWrap"><a class="button button-big" href="http://ads.mopub.com/inventory">Sign up now</a></span>
-              <span class="sep">&middot;</span>
-              <a href="/features">Learn more</a>
-            </div>
-          </div>
-          <div id="home-graphic">
-            <img src="placeholders/image.gif" alt="" width="475" height="270" />
-          </div>
+
+			<?php
+			global $post;
+			$myposts = get_posts( array( 'numberposts' => 1, 'offset'=> 0, 'post_parent' => 189, 'post_type' => 'page', 'orderby' => 'menu_order', 'order' => 'ASC' ) );
+			foreach( $myposts as $post ) :	setup_postdata($post); ?>
+          		<div id="home-intro">
+					<h1 id="home-headline"><?php the_title(); ?></h1>
+					<p><?php the_excerpt(); ?></p>
+
+		            <div id="home-cta">
+		              <span class="buttonWrap"><a class="button button-big" href="http://ads.mopub.com/inventory">Sign up now</a></span>
+		              <span class="sep">&middot;</span>
+		              <a href="/features">Learn more</a>
+		            </div>
+				</div>
+				
+				<?php				
+				$args = array( 'post_type' => 'attachment', 'numberposts' => -1, 'post_status' => null, 'post_parent' => $post->ID ); 
+				$attachments = get_posts($args);
+				if ($attachments) {
+					foreach ( $attachments as $attachment ) { ?>
+						
+						<div id="home-graphic">
+						  <img src="<?php echo wp_get_attachment_url($attachment->ID); ?>" alt="" width="475" height="270" />
+						</div>
+						
+				<?php
+					}
+				}?>				
+
+			<?php endforeach; ?>
+
         </div>
         
         <div id="home-banner">
           <span class="inner1"><span class="inner2"><span class="inner3">
-            <strong>1,234,567,890</strong> Impressions Served!
+            <strong>1B+</strong> Impressions Served! Yeah!
           </span></span></span>
         </div>
       </div>
       <div id="content">
         <div class="pageWidth">
           <section id="home-features">
-            <div class="home-feature" id="home-feature-1">
-              <img class="home-feature-graphic" src="images/home/home-networks.png" alt="" />
-              <h3>Run any ad</h3>
-              <p>
-                MoPub supports all types of ads, from banners to fullscreen video. Easily plug in any major ad network or use your own direct-sold or cross-promo content.
-              </p>
-            </div>
-            <div class="home-feature" id="home-feature-2">
-              <img class="home-feature-graphic" src="images/home/home-analytics.png" alt="" />
-              <h3>Measure everything</h3>
-              <p>
-                MoPub's powerful analytics let you track impressions, engagement, conversions, revenue, and more. Target the segments that matter with geographical breakdowns. 
-              </p>
-            </div>
-            <div class="home-feature" id="home-feature-3">
-              <img class="home-feature-graphic" src="images/home/home-platforms.png" alt="" />
-              <h3>Flexible, rock-solid platform</h3>
-              <p>
-                We know exactly what it's like to develop for the popular mobile platforms. You'll love working with our open source SDKs for iOS and Android.
-              </p>
-            </div>
+	
+			<?php
+			global $post;
+			$myposts = get_posts( array( 'numberposts' => 3, 'offset'=> 1, 'post_parent' => 189, 'post_type' => 'page', 'orderby' => 'menu_order', 'order' => 'ASC' ) );
+			foreach( $myposts as $post ) :	
+				setup_postdata($post); 
+				$attachments = get_posts(array( 'post_type' => 'attachment', 'numberposts' => -1, 'post_status' => null, 'post_parent' => $post->ID ));
+				if ($attachments) {
+					foreach ( $attachments as $attachment ) { ?>
+
+			            <div class="home-feature" id="home-feature-<?php echo $post->menu_order?>">
+			              <img class="home-feature-graphic" src="<?php echo wp_get_attachment_url($attachment->ID); ?>" alt="" width=290 height=150 />
+			              <h3><?php the_title(); ?></h3>
+			              <p>
+			                <?php the_excerpt(); ?>
+			              </p>
+			            </div>
+
+				<?php				
+					}
+				}
+			endforeach;	?>				
+				
           </section>
+
           <div class="clear"></div>
           <section id="mopub-public-cta">
             <span class="buttonWrap"><a class="button button-big" href="http://ads.mopub.com/inventory">Sign up now</a></span>

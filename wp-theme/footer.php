a88fb57239227f203807f185b2ad22fe670f858c
Jim <jpayne@alum.mit.edu>
2011-01-27 19:15:10 -0800

    - some tweaks to CSS styles for separation - footer links + copyright moving - separate product and company boxes

--- wp-theme/footer.php
+++ wp-theme/footer.php
@@ -5,14 +5,11 @@
 		  <div id="footer-inner">
 		    <div class="pageWidth">
 		      <div id="footer-links">
-		        <ul>
-		          <li>&copy; 2011 MoPub Inc.</li>
-				</ul>
-				  <?php wp_nav_menu( array( 'container_class' => '', 'container' => '', 'menu_class' => null, 'theme_location' => 'footer_left_nav' ) ); ?>
-				  <?php wp_nav_menu( array( 'container_class' => '', 'container' => null, 'menu_class' => 'footer_right_nav', 'theme_location' => 'footer_right_nav' ) ); ?>
+  			    <?php wp_nav_menu( array( 'container_class' => '', 'container' => '', 'menu_class' => null, 'theme_location' => 'footer_left_nav' ) ); ?>
+			    <?php wp_nav_menu( array( 'container_class' => '', 'container' => '', 'menu_class' => null, 'theme_location' => 'footer_right_nav' ) ); ?>
 		      </div>
 		      <div id="footer-account">
-		        
+				Copyright &copy; 2011 MoPub Inc.		        
 		      </div>
 		    </div>
 		  </div>
--- wp-theme/front-page.php
+++ wp-theme/front-page.php
@@ -1,6 +1,5 @@
 <?php get_header(); ?>
 
-
       <div id="titlebar">
         <div class="pageWidth">
 
@@ -36,19 +35,19 @@
 
         </div>
         
-        <div id="home-banner">
+        <!--div id="home-banner">
           <span class="inner1"><span class="inner2"><span class="inner3">
-            <strong>1+</strong> Impressions Served! Yeah!
+            <strong>NNN</strong> Impressions Served! Yeah!
           </span></span></span>
-        </div>
+        </div-->
       </div>
       <div id="content">
         <div class="pageWidth">
-          <section id="home-features">
-	
+		  <h2>Product Features</h2>
+          <section id="home-features">	
 			<?php
 			global $post;
-			$myposts = get_posts( array( 'numberposts' => 6, 'offset'=> 1, 'post_parent' => 189, 'post_type' => 'page', 'orderby' => 'menu_order', 'order' => 'ASC' ) );
+			$myposts = get_posts( array( 'numberposts' => 3, 'offset'=> 1, 'post_parent' => 189, 'post_type' => 'page', 'orderby' => 'menu_order', 'order' => 'ASC' ) );
 			foreach( $myposts as $post ) :	
 				setup_postdata($post); 
 				$attachments = get_posts(array( 'post_type' => 'attachment', 'numberposts' => -1, 'post_status' => null, 'post_parent' => $post->ID ));?>
@@ -69,11 +68,48 @@
 				          </section>
 
 			            </div>
-
 				<?php				
 			endforeach;	?>				
 				
           </section>
+		  <div class="hr"></div>
+		
+		  <h2>Company News</h2>
+          <section id="home-features-2">	
+			<?php
+			global $post;
+			$myposts = get_posts( array( 'numberposts' => 3, 'offset'=> 4, 'post_parent' => 189, 'post_type' => 'page', 'orderby' => 'menu_order', 'order' => 'ASC' ) );
+			foreach( $myposts as $post ) :	
+				setup_postdata($post); 
+				$attachments = get_posts(array( 'post_type' => 'attachment', 'numberposts' => -1, 'post_status' => null, 'post_parent' => $post->ID ));?>
+			            <div class="home-feature" id="home-feature-<?php echo ($post->menu_order - 1) % 3 + 1?>">
+						
+						  <?php if ($attachments) {
+								foreach ( $attachments as $attachment ) { ?>
+			              			<img class="home-feature-graphic" src="<?php echo wp_get_attachment_url($attachment->ID); ?>" alt="" width=290 height=150 />
+						  <?php } 
+							} ?>
+
+			              <h3><?php the_title(); ?></h3>
+			              <p>
+			                <?php the_excerpt(); ?>
+			              </p>
+				          <section>
+				            <a href="<?php echo get_post_meta($post->ID, 'more', true)?>">Learn more &raquo;</a>
+				          </section>
+
+			            </div>
+				<?php				
+			endforeach;	?>								
+          </section>
+
+          <div class="clear"></div>
+          <section id="mopub-public-cta">
+            <span class="buttonWrap"><a class="button button-big" href="http://ads.mopub.com/inventory">Sign up now</a></span>
+            <span class="sep">&middot;</span>
+            <a href="/features">Learn more</a>
+          </section>
+		
         </div>
       </div>
     

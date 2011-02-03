d37fd1b461638f277c62d2296d143ef113823ec4
Jim <jpayne@alum.mit.edu>
2011-02-02 19:35:24 -0800

    fix ad group page to have proper columnar layout

--- server/advertiser/templatetags/filters.py
+++ server/advertiser/templatetags/filters.py
@@ -6,39 +6,56 @@ import base64, binascii
 
 register = template.Library()
 
+@register.filter
 def currency(value):
   if value:
     return "$%s%s" % (withsep(int(value)), ("%0.2f" % value)[-3:])
   else:
     return "$0.00"
     
+@register.filter
 def currency_no_symbol(value):
   if value:
     return "%s%s" % (withsep(int(value)), ("%0.2f" % value)[-3:])
   else:
     return "0.00"
 
+@register.filter
 def percentage(value):
   if value:
   	return "%1.1f%%" % ((value or 0) * 100)
   else:
 	return "0.0%"	
   
+@register.filter
 def withsep(x):
   if x:
   	return re.sub(r'(\d{3})(?=\d)', r'\1,', str(x)[::-1])[::-1] 
   else:
 	return "0"
   
+@register.filter
 def format_date(value):
-  return value.strftime("%a, %b %d, %Y")
+  if value:
+    return value.strftime("%a, %b %d, %Y")
+  else:
+	return ""
+
+@register.filter
+def format_date_compact(value):
+  if value:
+    return value.strftime("%m/%d")
+  else:
+	return ""
   
+@register.filter
 def truncate(value, arg):
   if len(value) > arg:
     return "%s..." % value[:(arg-3)]
   else:
     return value
 
+@register.filter
 def time_ago_in_words(value):
   if value:
     if type(value) == str or type(value) == unicode:
@@ -62,6 +79,26 @@ def time_ago_in_words(value):
       return "%d days" % (d.days)
 
 @register.filter
+def campaign_status(c):
+  d = datetime.now()
+  if (c.start_date is None or d > c.start_date) and (c.end_date is None or d < c.end_date):
+	if c.budget: 
+	  if c.stats.revenue >= c.budget:
+	    return "Delivered"
+  	  elif c.stats.impression_count > 0:
+	    return "In-flight, %d%%" % (c.stats.revenue / float(c.budget))
+	  else:
+	    return "Eligible"
+	else:
+	  return "In-flight"
+  elif end_date and d > c.end_date:
+	return "Expired"
+  elif start_date and d < c.start_date:
+	return "Scheduled"
+  else:
+	return "Unknown"
+	
+@register.filter
 def all_user_dropdown(request,value=200):
   from django.utils.safestring import mark_safe
   from account.models import Account
@@ -76,10 +113,3 @@ def all_user_dropdown(request,value=200):
 def binary_data(data):
   return "data:image/png;base64,%s" % binascii.b2a_base64(data)
   
-register.filter(currency)
-register.filter(currency_no_symbol)
-register.filter(withsep)
-register.filter(percentage)
-register.filter(format_date)
-register.filter(truncate)
-register.filter(time_ago_in_words)

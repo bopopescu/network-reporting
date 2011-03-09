cbe0ae6cba11f80f4e8af8b5db183991e0a223b1
James Sun <james@mopub.com>
2011-03-09 10:59:31 -0800

    [#66] GAE datastore models for conversion tracking
    adding userstore containing GAE data models & query managers with unit tests [#66 state:review-requested assigned:nafis]
    
    GAE datastore for conversion tracking

---
+++ server/userstore/query_managers.py
@@ -0,0 +1,145 @@
+import logging
+from datetime import timedelta
+
+from google.appengine.ext import db
+
+from common.utils.cachedquerymanager import CachedQueryManager
+from userstore.models import MobileUser, MobileApp, ClickEvent, AppOpenEvent, CLICK_EVENT_NO_APP_ID, DEFAULT_CONVERSION_WINDOW
+
+
+NAMESPACE = None
+
+
+class MobileUserManager(CachedQueryManager):
+    Model = MobileUser
+
+    def get_mobile_user(self, udid):
+        return MobileUser.get(MobileUser.get_db_key(udid))
+    
+    def put_mobile_user(self, mobile_user):
+            return mobile_user.put()
+
+    @staticmethod
+    def get_or_insert(udid):
+        mobile_user_key = MobileUser.get_db_key(udid)
+        mobile_user = MobileUser.get(mobile_user_key)
+        if not mobile_user:
+            mobile_user = MobileUser(udid=udid)
+            mobile_user.put()
+        return mobile_user
+
+
+class MobileAppManager(CachedQueryManager):
+    Model = MobileApp
+
+    def get_mobile_apps(self, udid, mobile_appid=None, limit=50):
+        if mobile_appid:
+            mobile_app_db_key = MobileApp.get_db_key(udid, mobile_appid)
+            return [MobileApp.get(mobile_app_db_key)]
+        else:
+            return MobileApp.all().ancestor(MobileUser.get_db_key(udid)).fetch(limit)
+
+    def put_mobile_app(self, mobile_app):
+        return db.run_in_transaction(MobileAppManager._insert_mobile_app_transaction, mobile_app.udid, mobile_app.mobile_appid)
+
+    @staticmethod
+    def get_or_insert(udid, mobile_appid):
+        mobile_app_db_key = MobileApp.get_db_key(udid, mobile_appid)
+        mobile_app = MobileApp.get(mobile_app_db_key)
+        if not mobile_app:
+            return db.run_in_transaction(MobileAppManager._insert_mobile_app_transaction, udid, mobile_appid)
+        return mobile_app
+
+    @staticmethod
+    def _insert_mobile_app_transaction(udid, mobile_appid):
+        mobile_user = MobileUserManager.get_or_insert(udid)
+        mobile_app = MobileApp(udid=udid, mobile_appid=mobile_appid)
+        mobile_app.put()
+        return mobile_app
+    
+    
+class ClickEventManager(CachedQueryManager):
+    Model = ClickEvent
+
+    def get_click_events(self, udid=None, mobile_appid=None, \
+                             adunit=None, creative=None, \
+                             limit=50):
+        click_events = ClickEvent.all()
+        if udid:
+            click_events = click_events.filter('udid =', udid)
+        if mobile_appid:
+            click_events = click_events.filter('mobile_appid =', mobile_appid)
+        if adunit:
+            click_events = click_events.filter('adunit = ', adunit)
+        if creative:
+            click_events = click_events.filter('creative = ', creative)
+        return click_events.fetch(limit)
+
+    def log_click_event(self, udid, mobile_appid, time, adunit, creative):
+        return db.run_in_transaction(self._log_click_event_transaction, udid, mobile_appid, time, adunit, creative)
+        
+    def _log_click_event_transaction(self, udid, mobile_appid, time, adunit, creative):
+        # create click event
+        click_event = ClickEvent(udid=udid, mobile_appid=mobile_appid, time=time, adunit=adunit, creative=creative)
+        click_event.put()
+        # update corresponding mobile app with latest click info
+        if mobile_appid and mobile_appid != CLICK_EVENT_NO_APP_ID:
+            mobile_app_db_key = MobileApp.get_db_key(udid, mobile_appid)
+            mobile_app = MobileApp.get(mobile_app_db_key)
+            if not mobile_app:
+                mobile_user = MobileUserManager.get_or_insert(udid)
+                mobile_app = MobileApp(udid=udid, mobile_appid=mobile_appid)
+            mobile_app.latest_click_time = time
+            mobile_app.latest_click_adunit = adunit
+            mobile_app.latest_click_creative = creative
+            mobile_app.put()
+        return click_event
+                               
+        
+class AppOpenEventManager(CachedQueryManager):
+    Model = AppOpenEvent
+
+    def get_app_open_events(self, udid=None, mobile_appid=None, \
+                                conversion_adunit=None, conversion_creative=None, \
+                                limit=50):
+        app_open_events = AppOpenEvent.all()
+        if udid:
+            app_open_events = app_open_events.filter('udid = ', udid)
+        if mobile_appid:
+            app_open_events = app_open_events.filter('mobile_appid = ', mobile_appid)
+        if conversion_adunit:
+            app_open_events = app_open_events.filter('conversion_adunit = ', conversion_adunit)
+        if conversion_creative:
+            app_open_events = app_open_events.filter('conversion_creative = ', conversion_creative)
+        return app_open_events.fetch(limit)
+
+    def log_conversion(self, udid, mobile_appid, time, conversion_window=DEFAULT_CONVERSION_WINDOW):
+        return db.run_in_transaction(self._log_conversion_transaction, udid, mobile_appid, time, conversion_window)
+    
+    def _log_conversion_transaction(self, udid, mobile_appid, time, conversion_window):
+        # create app open event
+        app_open_event = AppOpenEvent(udid=udid, mobile_appid=mobile_appid, time=time, conversion_window=conversion_window)
+        
+        # get mobile app that was opened
+        mobile_app_db_key = MobileApp.get_db_key(udid, mobile_appid)
+        mobile_app = MobileApp.get(mobile_app_db_key)
+        if not mobile_app:
+                mobile_user = MobileUserManager.get_or_insert(udid)
+                mobile_app = MobileApp(udid=udid, mobile_appid=mobile_appid)
+                mobile_app.put()
+        elif mobile_app.latest_click_time:
+            # associate conversion adunit and creative if within window
+            if (time - mobile_app.latest_click_time) < timedelta(conversion_window):
+                app_open_event.conversion_adunit = mobile_app.latest_click_adunit
+                app_open_event.conversion_creative = mobile_app.latest_click_creative
+                app_open_event.put()
+                return app_open_event, True
+
+        app_open_event.put()
+        return app_open_event, False
+        
+            
+    
+    
+
+
---
+++ server/userstore/tests/models_mptests.py
@@ -0,0 +1,189 @@
+import sys
+import os
+import logging
+from datetime import datetime
+
+GAE_HOME = "/Applications/GoogleAppEngineLauncher.app/Contents/Resources/GoogleAppEngine-default.bundle/Contents/Resources/google_appengine"
+sys.path.append(GAE_HOME)
+sys.path.append(GAE_HOME+'/lib/yaml/lib')
+sys.path.append(GAE_HOME+'/lib/fancy_urllib')
+
+# configure your own src path
+SRC_HOME = os.environ['HOME']+'/src/server' 
+sys.path.append(SRC_HOME)
+
+from google.appengine.ext import db
+from nose.tools import assert_equals, assert_not_equals, assert_true, assert_raises
+
+from userstore.models import MobileUser, MobileApp, ClickEvent, AppOpenEvent, CLICK_EVENT_NO_APP_ID, DEFAULT_CONVERSION_WINDOW
+
+
+SAMPLE_UDID1 = 'udid1'
+SAMPLE_UDID2 = 'udid2'
+SAMPLE_MOBILE_APPID1 = 'mobile_appid1'
+SAMPLE_MOBILE_APPID2 = 'mobile_appid2'
+SAMPLE_ADUNIT1 = 'adunit1'
+SAMPLE_CREATIVE1 = 'creative1'
+
+
+def clear_datastore():
+    db.delete(MobileUser.all())
+    db.delete(MobileApp.all())
+    db.delete(ClickEvent.all())
+    db.delete(AppOpenEvent.all())
+
+    
+def mobile_user_model_mptests():
+    # creation
+    user = MobileUser(udid=SAMPLE_UDID1)
+    user.put()
+
+    # fetch count check
+    assert_equals(MobileUser.all().count(), 1)
+    fetched_user = MobileUser.all().fetch(1)[0]
+
+    # udid and key check
+    assert_equals(fetched_user.udid, SAMPLE_UDID1)
+    assert_equals(fetched_user.udid, user.udid)
+    assert_equals(fetched_user.key(), user.key())
+
+    # parent key check
+    assert_true(fetched_user.parent_key() is None)
+    
+    # deletion
+    user.delete()
+
+    # fetch count check again
+    assert_equals(MobileUser.all().count(), 0)
+
+    # cleanup
+    clear_datastore()
+
+    
+def mobile_app_model_mptests():
+    # required properties check
+    assert_raises(NameError, MobileApp)
+    assert_raises(NameError, MobileApp, udid=SAMPLE_UDID1)
+    assert_raises(NameError, MobileApp, mobile_appid=SAMPLE_MOBILE_APPID1)
+
+    # creation
+    app = MobileApp(udid=SAMPLE_UDID1, mobile_appid=SAMPLE_MOBILE_APPID1)
+    app.put()
+
+     # fetch count check
+    assert_equals(MobileApp.all().count(), 1)
+    fetched_app = MobileApp.all().fetch(1)[0]
+
+    # udid and key check
+    assert_equals(fetched_app.udid, SAMPLE_UDID1)
+    assert_equals(fetched_app.mobile_appid, SAMPLE_MOBILE_APPID1)
+    assert_equals(fetched_app.key(), app.key())
+    
+    # parent key check
+    assert_true(fetched_app.parent_key() is not None)
+
+    # cleanup
+    clear_datastore()
+
+
+def click_event_model_mptests():
+    #initialization
+    dt = datetime.now()
+    
+    # required properties check
+    assert_raises(NameError, ClickEvent)
+    assert_raises(NameError, ClickEvent,
+                  udid=SAMPLE_UDID1, time=dt, adunit=SAMPLE_ADUNIT1) # missing creative
+    assert_raises(NameError, ClickEvent,
+                  udid=SAMPLE_UDID1, time=dt, creative=SAMPLE_CREATIVE1) # missing adunit
+    assert_raises(NameError, ClickEvent,
+                  udid=SAMPLE_UDID1, adunit=SAMPLE_ADUNIT1, creative=SAMPLE_CREATIVE1) # missing time
+    assert_raises(NameError, ClickEvent,
+                  time=dt, adunit=SAMPLE_ADUNIT1, creative=SAMPLE_CREATIVE1) # missing udid
+
+    # creation, one with mobile_appid and one without
+    ce1 = ClickEvent(udid=SAMPLE_UDID1, time=dt, adunit=SAMPLE_ADUNIT1, creative=SAMPLE_CREATIVE1)
+    ce2 = ClickEvent(udid=SAMPLE_UDID1, mobile_appid=SAMPLE_MOBILE_APPID1, time=dt, adunit=SAMPLE_ADUNIT1, creative=SAMPLE_CREATIVE1)
+    db.put([ce1, ce2])
+    
+     # fetch count check
+    assert_equals(ClickEvent.all().count(), 2)
+    fetched_ce1 = ClickEvent.all().fetch(2)[0]
+    fetched_ce2 = ClickEvent.all().fetch(2)[1]
+    
+    # udid and key check
+    assert_equals(fetched_ce1.udid, SAMPLE_UDID1)
+    assert_equals(fetched_ce1.mobile_appid, CLICK_EVENT_NO_APP_ID)
+    assert_equals(fetched_ce2.mobile_appid, SAMPLE_MOBILE_APPID1)
+    assert_equals(fetched_ce1.time, dt)
+    assert_equals(fetched_ce1.adunit, SAMPLE_ADUNIT1)
+    assert_equals(fetched_ce1.creative, SAMPLE_CREATIVE1)
+    assert_equals(fetched_ce1.key(), ce1.key())
+    assert_equals(fetched_ce2.key(), ce2.key())
+    assert_not_equals(ce1.key(), ce2.key())
+
+    # parent key check
+    assert_true(fetched_ce1.parent_key() is not None)
+    assert_true(fetched_ce2.parent_key() is not None)
+    assert_not_equals(fetched_ce1.parent_key(), fetched_ce2.parent_key())
+    
+    # cleanup
+    clear_datastore()
+
+
+def app_open_event_model_mptests():
+    # initialization
+    dt = datetime.now()
+
+    # required properties check
+    assert_raises(NameError, AppOpenEvent)
+    assert_raises(NameError, AppOpenEvent,
+                  udid=SAMPLE_UDID1, mobile_app=SAMPLE_MOBILE_APPID1) # missing time
+    assert_raises(NameError, AppOpenEvent,
+                  udid=SAMPLE_UDID1, time=dt) # missing mobile_appid
+    assert_raises(NameError, AppOpenEvent,
+                  mobile_app=SAMPLE_MOBILE_APPID1, time=dt) # missing udid
+
+    # creation
+    aoe1 = AppOpenEvent(udid=SAMPLE_UDID1, mobile_appid=SAMPLE_MOBILE_APPID1, time=dt)
+    aoe2 = AppOpenEvent(udid=SAMPLE_UDID2, mobile_appid=SAMPLE_MOBILE_APPID2, time=dt, conversion_window=2)
+    db.put([aoe1, aoe2])
+
+    # fetch count check
+    assert_equals(AppOpenEvent.all().count(), 2)
+    fetched_aoe1 = AppOpenEvent.all().fetch(2)[0]
+    fetched_aoe2 = AppOpenEvent.all().fetch(2)[1]
+    
+    # properties check
+    assert_equals(fetched_aoe1.udid, SAMPLE_UDID1)
+    assert_equals(fetched_aoe1.mobile_appid, SAMPLE_MOBILE_APPID1)
+    assert_equals(fetched_aoe1.time, dt)
+    assert_equals(fetched_aoe1.conversion_window, DEFAULT_CONVERSION_WINDOW)
+
+    assert_equals(fetched_aoe2.udid, SAMPLE_UDID2)
+    assert_equals(fetched_aoe2.mobile_appid, SAMPLE_MOBILE_APPID2)
+    assert_equals(fetched_aoe2.time, dt)
+    assert_equals(fetched_aoe2.conversion_window, 2)
+
+    assert_equals(fetched_aoe1.key(), aoe1.key())
+    assert_equals(fetched_aoe2.key(), aoe2.key())
+    assert_not_equals(aoe1.key(), aoe2.key())
+
+    # parent key check
+    assert_true(fetched_aoe1.parent_key() is not None)
+    assert_true(fetched_aoe2.parent_key() is not None)
+    assert_not_equals(fetched_aoe1.parent_key(), fetched_aoe2.parent_key())
+
+    # cleanup
+    clear_datastore()
+
+
+    
+                  
+    
+
+
+
+    
+
+

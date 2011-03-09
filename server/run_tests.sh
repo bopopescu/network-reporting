6ab1e7bb45d39421863ab22ade056891df56f641
James Sun <james@mopub.com>
2011-03-09 10:59:31 -0800

    integration with logging service; minor mod to download.sh [#91 state:review-requested assigned:nafis]
    adding userstore containing GAE data models & query managers with unit tests [#66 state:review-requested assigned:nafis]
    
    conversion tracking integration with new logging system [#91 state:review-requested assigned:nafis]
    
    conversion tracking integration with new logging system [#91 state:review-requested assigned:nafis]
    
    conversion tracking integration with new logging [#91 state:review-requested assigned:nafis]
    
    added app.yaml back
    
    integration with logging server; minor mod to download.sh

--- server/run_tests.sh
+++ server/run_tests.sh
@@ -4,11 +4,11 @@ echo $1
 if [ -z "$1" ] # if $1 does not exist
 then
     nosetests --with-gae --gae-datastore='./test_data/basic_test.datastore' --with-coverage ad_server_tests
+    nosetests --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' --where='./userstore/tests' --with-coverage
 else
     nosetests --with-gae --gae-datastore='./test_data/basic_test.datastore' --gae-lib-root="$1" --with-coverage --with-xunit --verbose ad_server_tests
+    nosetests --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' --gae-lib-root="$1" --where='./userstore/tests' --with-coverage
 fi
 
-# GAE data modeling for conversion tracking
-#nosetests --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' --where='./userstore/tests' --with-coverage
 
-# --with-profile --profile-stats-file='./test_data/profile_statsi.stats'
+

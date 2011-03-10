abfdd205f683e9373a32f285d03cad20a46611f5
Nafis Jamal <nafisjamal@mopub.com>
2011-03-10 00:04:36 -0800

    created a new way to detect the existance of environment variable

--- server/run_tests.sh
+++ server/run_tests.sh
@@ -1,13 +1,14 @@
+#!/bin/sh
+
 echo $1
-if [ "x" == "x$1" ] # if $1 does not exist
+if [ -z "$1" ] # if $1 does not exist
 then
     nosetests --with-gae --gae-datastore='./test_data/basic_test.datastore' --with-coverage ad_server_tests
 else
-    nosetests --with-gae --gae-datastore='./test_data/basic_test.datastore' --gae-lib-root='$1' --with-coverage ad_server_tests
+    nosetests --with-gae --gae-datastore='./test_data/basic_test.datastore' --gae-lib-root="$1" --with-coverage ad_server_tests
 fi
 
-
 # GAE data modeling for conversion tracking
-nosetests --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' --where='./userstore/tests' --with-coverage
+#nosetests --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' --where='./userstore/tests' --with-coverage
 
 # --with-profile --profile-stats-file='./test_data/profile_statsi.stats'

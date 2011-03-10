5a6190817f9608a560a4c27d56dd5cac433c8428
Nafis Jamal <nafisjamal@mopub.com>
2011-03-10 00:20:02 -0800

    added flags to nose

--- server/run_tests.sh
+++ server/run_tests.sh
@@ -5,7 +5,7 @@ if [ -z "$1" ] # if $1 does not exist
 then
     nosetests --with-gae --gae-datastore='./test_data/basic_test.datastore' --with-coverage ad_server_tests
 else
-    nosetests --with-gae --gae-datastore='./test_data/basic_test.datastore' --gae-lib-root="$1" --with-coverage ad_server_tests
+    nosetests --with-gae --gae-datastore='./test_data/basic_test.datastore' --gae-lib-root="$1" --with-coverage --with-xunit --verbose --ad_server_tests
 fi
 
 # GAE data modeling for conversion tracking

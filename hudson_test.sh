f9cbe93c738123b24b76711dcc738de2f9e8ba2d
Nafis Jamal <nafisjamal@mopub.com>
2011-03-09 23:38:54 -0800

    added hudson testing script and fixed run_tests.sh

---
+++ hudson_test.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd "$1"
+./run_tests.sh "/home/nafis/src/google_appengine/"
--- server/run_tests.sh
+++ server/run_tests.sh
@@ -1,4 +1,10 @@
-nosetests --with-gae --gae-datastore='./test_data/basic_test.datastore' --gae-lib-root='/home/nafis/src/google_appengine/' --with-coverage ad_server_tests
+if [ "x" == "x$1" ] # if $1 does not exist
+then
+    nosetests --with-gae --gae-datastore='./test_data/basic_test.datastore' --with-coverage ad_server_tests
+else
+    nosetests --with-gae --gae-datastore='./test_data/basic_test.datastore' --gae-lib-root='$1' --with-coverage ad_server_tests
+fi
+
 
 # GAE data modeling for conversion tracking
 nosetests --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' --where='./userstore/tests' --with-coverage

67cafa66e5767f2827bf844da38dbe86354b65b3
James Sun <james@mopub.com>
2011-03-11 12:26:25 -0800

    put back load django in main.py

--- server/ad_server/main.py
+++ server/ad_server/main.py
@@ -1,14 +1,14 @@
 # !/usr/bin/env python
 
 # TODO: PLEASE HAVE THIS FIX DJANGO PROBLEMS
-#from appengine_django import LoadDjango
-#LoadDjango()
-#import os
-#from django.conf import settings
+from appengine_django import LoadDjango
+LoadDjango()
+import os
+from django.conf import settings
 
-#os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 # Force Django to reload its settings.
-#settings._target = None
+settings._target = None
 # END TODO: PLEASE HAVE THIS FIX DJANGO PROBLEMS
 
 import wsgiref.handlers

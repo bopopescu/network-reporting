cb0f10d3c526223605d047792b153310dfea2e66
nottombrown <nottombrown@gmail.com>
2011-03-14 15:32:13 -0700

    added basic budget service

---
+++ server/ad_server/budget/budget_service.py
@@ -0,0 +1,50 @@
+
+class BudgetService(object):
+    """
+    A service that determines if a campaign can be shown based upon the defined 
+    budget for that campaign. If the budget_type is "evenly", a minute by minute
+    sub-budget is kept as well. 
+    """
+
+    def __init__(self, campaign_id, daily_budget, timeslices=1440, fudge_factor=0.0, test_mode=False):
+        self.campaign_id = campaign_id
+        self.remaining_daily_budget = daily_budget
+        self.remaining_timeslices = timeslices
+        self.fudge_factor = fudge_factor
+        self.budget_timeslices = {}
+        self.counter = 0
+        
+        self._set_next_timeslice()
+
+    def _update_budget(self, amount_spent):
+        self.budget_timeslices[self.timeslice_name] = self.budget_timeslices[self.timeslice_name] - amount_spent
+  
+    def _next_timeslice_budget(self):
+            return (self.remaining_daily_budget / self.remaining_timeslices) * (1 + self.fudge_factor)
+            
+    def _set_next_timeslice(self):
+        rollover_budget = self._get_timeslice_budget()
+
+        #set new timeslice name
+        self.timeslice_name = str(self.campaign_id) + ":" + str(self.counter)
+        self.counter += 1
+        
+        self.budget_timeslices[self.timeslice_name] = rollover_budget + self._next_timeslice_budget()
+        
+    def _has_budget(self):
+        if self._get_timeslice_budget() >= 0:
+            return True
+        return False
+        
+    def _get_timeslice_budget(self):
+        try:
+            return self.budget_timeslices[self.timeslice_name]
+        except AttributeError:
+            return 0
+        
+    def process(self, amount_spent):
+        """ Return true if the current timeslice has a budget for a given cost"""
+        self._update_budget(amount_spent)
+        return self._has_budget()
+            
+        
---
+++ server/ad_server/budget/budget_service_tests.py
@@ -0,0 +1,41 @@
+from nose.tools import eq_
+from nose.tools import with_setup
+from server.ad_server.budget.budget_service import BudgetService
+
+def test_budget_all_at_once():
+    budget_service = BudgetService(1,500,timeslices=1,test_mode=True)
+    eq_(budget_service.process(300),True)
+    eq_(budget_service.process(300),False)
+
+def test_budget_evenly_success():
+    budget_service = BudgetService(1,500,timeslices=2,test_mode=True)
+    eq_(budget_service.process(125),True)
+    eq_(budget_service.process(125),True)
+    eq_(budget_service._get_timeslice_budget(),0)
+    eq_(budget_service.process(125),False)
+
+  
+def test_budget_evenly_failure():
+    budget_service = BudgetService(1,500,timeslices=2,test_mode=True)
+    eq_(budget_service.process(300),False)
+    eq_(budget_service._get_timeslice_budget(),-50)
+    eq_(budget_service.process(300),False)
+
+def test_multiple_timeslices():
+    budget_service = BudgetService(1,200,timeslices=2,test_mode=True)
+    eq_(budget_service._get_timeslice_budget(),100)
+    eq_(budget_service.process(100),True)
+    
+    budget_service._set_next_timeslice()
+    eq_(budget_service._get_timeslice_budget(),100)
+    eq_(budget_service.process(100),True)
+ 
+def test_multiple_timeslice_rollover():
+    budget_service = BudgetService(1,200,timeslices=2,test_mode=True)
+
+    budget_service._set_next_timeslice()
+    
+    eq_(budget_service._get_timeslice_budget(),200)
+    eq_(budget_service.process(100),True)
+    eq_(budget_service._get_timeslice_budget(),100)
+    eq_(budget_service.process(100),True)

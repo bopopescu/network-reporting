0ecc9d9726171d0ff3c847d530d7093462c9afa6
nottombrown <nottombrown@gmail.com>
2011-03-15 16:43:46 -0700

    passing tests

--- server/advertiser/models.py
+++ server/advertiser/models.py
@@ -14,6 +14,9 @@ class Campaign(db.Model):
   # daily budget
   budget = db.FloatProperty() 
   
+  # remaining daily budget
+  remaining_budget = db.FloatProperty()
+  
   # start and end dates 
   start_date = db.DateProperty()
   end_date = db.DateProperty()
@@ -56,9 +59,6 @@ class AdGroup(db.Model):
   
   # daily budget
   budget = db.FloatProperty() 
-  
-  # remaining daily budget
-  remaining_budget = db.FloatProperty()
   
   # start and end dates 
   start_date = db.DateProperty()

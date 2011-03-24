3199f7ce4da35df1e4613f8bf880db654b893022
nottombrown <nottombrown@gmail.com>
2011-03-23 17:47:51 -0700

    fixed datastore bug

--- server/run_tests.sh
+++ server/run_tests.sh
@@ -3,7 +3,7 @@
 echo $1
 if [ -z "$1" ] # if $1 does not exist
 then
-    # nosetests --with-gae --gae-datastore='./test_data/basic_test.datastore' ad_server_tests # --with-coverage
+    nosetests --with-gae --gae-datastore='./test_data/basic_test.datastore' ad_server_tests # --with-coverage
     # nosetests --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' --where='./userstore/tests' --with-coverage
     # nosetests --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' --where='./reporting/tests' --with-coverage  --without-sandbox 
     nosetests --match='(?:^|[\b_\./-])mptest' --with-gae --gae-datastore='./test_data/basic_test.datastore' budget_integration_mptests #--with-coverage 

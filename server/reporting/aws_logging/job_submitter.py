6f856a8a9cb71b8f4065afb16d0ac72541dd908e
James Sun <james@mopub.com>
2011-03-31 17:41:06 -0700

    offline logging system EC2 integration [#124 state:review-requested assigned:nafis]

--- server/reporting/aws_logging/job_submitter.py
+++ server/reporting/aws_logging/job_submitter.py
@@ -1,27 +1,34 @@
 import logging
-import sys
 import time
 from optparse import OptionParser
 
-import boto
-import boto.emr
+from boto.emr.connection import EmrConnection
 from boto.emr.step import StreamingStep
 
 
-S3_BUCKET = 'log-counts'
+S3_BUCKET = 'mopub-aws-logging'
 LOG_URI = 's3://' + S3_BUCKET + '/jobflow_logs'
-# MAPPER = 's3://' + S3_BUCKET + '/code/logparser_mapper.py'
-# REDUCER = 'aggregate'
 MAPPER = 's3://' + S3_BUCKET + '/code/log_mapper.py'
 REDUCER = 's3://' + S3_BUCKET + '/code/log_reducer.py'
 
 
-
+JOBFLOW_FILE = 'jobflow.txt'
 NUM_INSTANCES = 1
 MASTER_INSTANCE_TYPE = 'm1.small'
 SLAVE_INSTANCE_TYPE = 'm1.small'
-KEEP_ALIVE = False
+KEEP_ALIVE = True
     
+
+
+def get_waiting_jobflow(conn):
+    waiting_jobflow_ids = conn.describe_jobflows([u'WAITING'])
+    if len(waiting_jobflow_ids) > 0:
+        jobid = waiting_jobflow_ids[0].jobflowid
+        print 'found waiting jobflow:', jobid
+        return jobid
+    else:
+        return None
+
     
 def main():
     start = time.time()
@@ -31,7 +38,7 @@ def main():
     parser.add_option('-n', '--num_instances', dest='num_instances', default=NUM_INSTANCES)
     (options, args) = parser.parse_args()
     
-    conn = boto.connect_emr()
+    conn = EmrConnection('AKIAJKOJXDCZA3VYXP3Q', 'yjMKFo61W0mMYhMgphqa+Lc2WX74+g9fP+FVeyoH')
 
     step = StreamingStep(
         name='logparser step',
@@ -40,27 +47,39 @@ def main():
         input=options.logfile,
         output=options.logfile+'.out',
     )
-
-    jobid = conn.run_jobflow(
-        name='logparser job',
-        steps=[step],
-        log_uri=LOG_URI,
-        num_instances=options.num_instances,
-        master_instance_type=MASTER_INSTANCE_TYPE,
-        slave_instance_type=SLAVE_INSTANCE_TYPE,
-        keep_alive=KEEP_ALIVE,
-        enable_debugging=True,
-    )
-
-    print 'submitted job: %s'%jobid
-
+    
+    # try to find an existing jobflow in waiting mode
+    jobid = get_waiting_jobflow(conn)
+    if jobid:
+        conn.add_jobflow_steps(jobid, [step])
+        print 'added step to waiting jobflow:', jobid
+        
+        # wait while jobflow is still in waiting mode
+        while conn.describe_jobflow(jobid).state == u'WAITING':
+            print '%s\tjob state: WAITING' % time.strftime('%b %d %Y %H:%M:%S')
+            time.sleep(1)
+    else:   # spin up a new jobflow    
+        jobid = conn.run_jobflow(
+            name='logparser job',
+            steps=[step],
+            log_uri=LOG_URI,
+            num_instances=options.num_instances,
+            master_instance_type=MASTER_INSTANCE_TYPE,
+            slave_instance_type=SLAVE_INSTANCE_TYPE,
+            keep_alive=KEEP_ALIVE,
+            enable_debugging=True,
+        )
+        print 'submitted new jobflow:', jobid
+        
+    # check jobflow status periodically while it's active
     while True:
         state = conn.describe_jobflow(jobid).state
         print '%s\tjob state: %s' % (time.strftime('%b %d %Y %H:%M:%S'), state)
-        if state not in [u'COMPLETED', u'FAILED', u'TERMINATED']:
-            time.sleep(30)
-        else:
+        if state in [u'COMPLETED', u'FAILED', u'TERMINATED', u'WAITING']:
             break
+        else:
+            time.sleep(10)
+
     
     elapsed = time.time() - start
     print "job %s took %i minutes and %i seconds" % (jobid, elapsed/60, elapsed%60)
--- server/reporting/aws_logging/run_jobflow.sh
+++ server/reporting/aws_logging/run_jobflow.sh
@@ -1,52 +1,109 @@
 #!/bin/bash
-PATH=$PATH:~/google_appengine
+PATH=$PATH:/usr/local/bin:~/google_appengine
+
+APP_DIR=~/mopub/server
+LOG_DIR=~/aws_logs
+S3_BUCKET=mopub-aws-logging
+S3_LOG_DIR=s3://$S3_BUCKET/logs-`date +"%Y-%m%d"`
+
+LOGFILE=aws-logfile-`date +"%Y-%m%d-%H%M"`
+LOCAL_LOGFILE=$LOG_DIR/$LOGFILE
+S3_LOGFILE=$S3_LOG_DIR/$LOGFILE
 
 
-# set AWS credentials for boto
-export AWS_ACCESS_KEY_ID=AKIAJKOJXDCZA3VYXP3Q
-export AWS_SECRET_ACCESS_KEY=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
+# start timestamp
+OVERALL_START_TIME=$(date +%s)
+echo
+echo
+echo `date +"%D"` `date +"%T"`
+echo
+echo
+
+mkdir $LOG_DIR
+
+START_TIME=$(date +%s)
 
 # download logs from GAE
-echo N47935 | appcfg.py --version=$1 --no_cookies --email=olp@mopub.com --passin --append --num_days=1 request_logs ~/src/server ~/logs/request-logfile-1
-echo
-echo N47935 | appcfg.py --version=$2 --no_cookies --email=olp@mopub.com --passin --append --num_days=1 request_logs ~/src/server ~/logs/request-logfile-2
+echo N47935 | appcfg.py --no_cookies --email=olp@mopub.com --passin --append --num_days=1 request_logs $APP_DIR $LOG_DIR/request-logfile
 echo
 
-# combine logfiles
+STOP_TIME=$(date +%s)
 echo
-echo "merging log files to" $LOCAL_LOGFILE "..."
-#LOGFILE=logfile-`date +"%Y%m%d-%H%M%S"`
-LOGFILE=aws-logfile-`date +"%Y%m%d"`
-LOCAL_LOGFILE=~/logs/$LOGFILE
-S3_LOGFILE=s3://log-counts/test/logs/$LOGFILE
-cat ~/logs/request-logfile-1 ~/logs/request-logfile-2 > $LOCAL_LOGFILE
+echo "downloading GAE logs took" $((STOP_TIME-START_TIME)) "seconds"
+
+
+echo
+echo "copying downloaded logs to" $LOCAL_LOGFILE "as input to EMR..."
+cp $LOG_DIR/request-logfile $LOCAL_LOGFILE
+
+
+START_TIME=$(date +%s)
 
 # upload new logs to S3
-echo 
-echo "uploading to S3..."
+echo
+echo "uploading" $LOCAL_LOGFILE "to" $S3_LOGFILE "..."
 s3cmd put $LOCAL_LOGFILE $S3_LOGFILE
+
+STOP_TIME=$(date +%s)
+echo "uploading GAE logs to S3 took" $((STOP_TIME-START_TIME)) "seconds"
+
+
+START_TIME=$(date +%s)
 
 # submit and run job
 echo
-python job_submitter.py -f $S3_LOGFILE #-n 3
+echo "submitting EMR job..."
+python $APP_DIR/reporting/aws_logging/job_submitter.py -f $S3_LOGFILE #-n 3
+
+STOP_TIME=$(date +%s)
+echo "EMR job took" $((STOP_TIME-START_TIME)) "seconds"
+
+
+START_TIME=$(date +%s)
 
 # download output files from S3 and merge them into one
-echo 
-echo "making tmp directory..."
-mkdir tmp
-echo "downloading output files from S3..."
-s3cmd get $S3_LOGFILE.out/part-* tmp
-echo "merging output files to" $LOCAL_LOGFILE.out "..."
-cat tmp/part-* > $LOCAL_LOGFILE.out
-echo "removing tmp directory..."
-rm -rf tmp
+echo
+s3cmd get $S3_LOGFILE.out/part-* $LOG_DIR
+
+STOP_TIME=$(date +%s)
+echo "download S3 output files took" $((STOP_TIME-START_TIME)) "seconds"
+
+echo
+echo "merging output files to" $LOCAL_LOGFILE.stats "..."
+cat $LOG_DIR/part-* > $LOCAL_LOGFILE.stats
+
+
+# deleting local and remote S3 output files (clean slate for next run)
+echo
+echo "deleting local S3 part files at" $LOG_DIR
+rm -rf $LOG_DIR/part-*
+echo "deleting remote S3 part files at" $S3_LOGFILE.out
+s3cmd del --recursive $S3_LOGFILE.out
+
+
+START_TIME=$(date +%s)
 
 # parse MR output and update StatsModels in GAE datastore
-python stats_updater.py -f $LOCAL_LOGFILE.out 
+echo
+echo "updating GAE datastore..."
+python $APP_DIR/reporting/aws_logging/stats_updater.py -f $LOCAL_LOGFILE.stats 
+
+STOP_TIME=$(date +%s)
+echo "updating GAE datastore took" $((STOP_TIME-START_TIME)) "seconds"
 
 
+echo
+echo "uploading stats file to" $S3_LOGFILE.stats
+s3cmd put $LOCAL_LOGFILE.stats $S3_LOGFILE.stats
 
 
-#echo; START_TIME=$(date +%s); echo "START="$START_TIME; echo; python stats_updater.py -f ~/logs/logfile-20110324-191951.out; STOP_TIME=$(date +%s); echo "STOP =" $STOP_TIME; echo "=========";echo "LATENCY=" $((STOP_TIME-START_TIME));
-
+# end timestamp
+OVERALL_STOP_TIME=$(date +%s)
+echo
+echo
+echo "total time:" $(((OVERALL_STOP_TIME-OVERALL_START_TIME)/60)) "minutes and" $(((OVERALL_STOP_TIME-OVERALL_START_TIME)%60)) "seconds"
+echo
+echo `date +"%D"` `date +"%T"`
+echo
+echo
 

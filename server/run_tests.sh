cc298b53eee23c7851a32595738071867e28444f
Nick Grabenstein <nick@mopub.com>
2011-04-18 08:13:27 -0700

    Added a test

--- server/run_tests.sh
+++ server/run_tests.sh
@@ -9,6 +9,7 @@ then
     nosetests --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' --where='./ad_server/filters/tests' --without-sandbox # --with-coverage  
     nosetests --match='(?:^|[\b_\./-])mptest' --with-gae --gae-datastore='./test_data/basic_test.datastore' budget_integration_mptests
     nosetests --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' --where='./ad_server/optimizer/tests' --without-sandbox
+    nosetests --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' --where='./reports/tests' --without-sandbox # --with-coverage  
 else
     nosetests --with-gae --gae-datastore='./test_data/basic_test.datastore' --gae-lib-root="$1" --with-coverage --with-xunit --verbose ad_server_tests
     nosetests --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' --gae-lib-root="$1" --where='./userstore/tests' --with-coverage

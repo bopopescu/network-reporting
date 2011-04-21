1c8d42aa9c24e3121c95e40c44048c7d23d3b1c1
Jim <jpayne@alum.mit.edu>
2011-04-20 18:02:48 -0700

    -- introducing beatbox for SFDC -- to_sfdc method -- new cron task in admin

--- server/account/models.py
+++ server/account/models.py
@@ -30,7 +30,21 @@ class Account(db.Model):
     
     def is_admin(self):
         return users.is_current_user_admin()
-
+        
+    def to_sfdc(self):
+        return {'Company': self.name, 
+                'FirstName': "A", 'LastName': "Developer",
+                'LeadSource': 'AppStoreCrawl', 
+                'Number_of_Apps__c': len(self.apps),
+                'Apps__c': "\n".join(["%s (#%d in %s)" % (a.get("title"), a.get("rank"), a.get("category")) for a in self.apps]),
+                'iTunesURL__c': max([a.get("url") for a in self.apps]),
+                'Top_Rank__c': min(a.get('rank') for a in self.apps),
+                'Primary_Category__c': max(set(categories), key=categories.count),
+                'iTunes_Artist_Name__c': max([a.get('artist') for a in self.apps]),
+                'HtmlSummary__c': "<hr/>".join([a.get('summary') for a in self.apps]),
+                'Description': '',
+                'type': 'Lead'}
+        
     def __eq__(self, other):
         if other:
             return str(self.key()) == str(other.key())

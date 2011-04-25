a140d709d7837bbefb92e8fc62834ef26c673027
Nafis Jamal <nafisjamal@mopub.com>
2011-04-25 11:36:45 -0700

    now escaping account key to appropriate name the task

--- server/mopub_logging/mp_logging.py
+++ server/mopub_logging/mp_logging.py
@@ -113,7 +113,8 @@ def log(request,event,adunit=None,creative=None,manager=None,adunit_id=None,crea
     memcache.set(log_key,logging_data,time=MEMCACHE_ALIVE_TIME)
     
     # send to appropriately named task_queue
-    task_name = TASK_NAME%dict(account_name=account_name,time=time_bucket)
+    task_name = TASK_NAME%dict(account_name=account_name.replace('_','1X--X1'), # must escape '_' regex: [a-zA-Z0-9-]{1,500}$   
+                               time=time_bucket)
     
     try:
         account_bucket = hash(account_name)%NUM_TASK_QUEUES

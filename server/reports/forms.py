3bd1aacb980242bec53a448c17983d2d5ae53f73
nottombrown <nottombrown@gmail.com>
2011-04-26 19:46:47 -0700

    made forms for graben

--- server/reports/forms.py
+++ server/reports/forms.py
@@ -1,10 +1,9 @@
 import urllib2
-import loggin
+import logging
 
 from google.appengine.ext import db
 
 from django import forms
-from django.cor.urlresolvers import reverse
 from common.utils import forms as mpforms
 from common.utils import fields as mpfields
 from common.utils import widgets as mpwidgets
@@ -14,6 +13,9 @@ from reports.models import Report
 class ReportForm(mpforms.MPModelForm):
     TEMPLATE = 'reports/forms/report_form.html'
 
+    d1 = mpfields.MPChoiceField(choices=[('normal','Normal'),('backfill','Backfill')],widget=mpwidgets.MPSelectWidget)
+    d2 = mpfields.MPChoiceField(choices=[('normal','Normal'),('backfill','Backfill')],widget=mpwidgets.MPSelectWidget)
+    d3 = mpfields.MPChoiceField(choices=[('normal','Normal'),('backfill','Backfill')],widget=mpwidgets.MPSelectWidget)
     class Meta:
         model = Report
         fields = ('d1', 'd2', 'd3', 'start', 'end', 'name')

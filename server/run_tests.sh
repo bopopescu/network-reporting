b9c3d52715859ff09c9847122f422299881748b5
Nafis Jamal <nafisjamal@mopub.com>
2011-05-16 16:28:43 -0700

    update run test

--- server/run_tests.sh
+++ server/run_tests.sh
@@ -18,12 +18,19 @@ then
     nosetests --with-gae --gae-datastore='./test_data/basic_test.datastore' ad_server_tests # --with-coverage
     
 else
-    nosetests --with-gae --gae-datastore='./test_data/basic_test.datastore' --gae-lib-root="$1" --with-coverage --with-xunit --verbose ad_server_tests
-    nosetests --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' --gae-lib-root="$1" --where='./userstore/tests' --with-coverage
-    nosetests --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' --gae-lib-root="$1" --where='./reporting/tests' --with-coverage --without-sandbox
-    nosetests --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' --gae-lib-root="$1" --where='./ad_server/filters/tests' --with-coverage  --without-sandbox 
-    nosetests --match='(?:^|[\b_\./-])mptest' --with-gae --gae-datastore='./test_data/basic_test.datastore' --gae-lib-root="$1" budget_integration_mptests
-    nosetests --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' --where='./ad_server/optimizer/tests' --gae-lib-root="$1" --without-sandbox    
+    nosetests --gae-lib-root="$1" --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' --where='./userstore/tests' #--with-coverage
+    nosetests --gae-lib-root="$1" --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' --where='./reporting/tests' --without-sandbox # --with-coverage 
+    nosetests --gae-lib-root="$1" --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' --where='./ad_server/filters/tests' --without-sandbox # --with-coverage  
+    nosetests --gae-lib-root="$1" --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' --where='./budget/tests' --without-sandbox
+    nosetests --gae-lib-root="$1" --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' --where='./ad_server/optimizer/tests' --without-sandbox
+    nosetests --gae-lib-root="$1" --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' --where='./common/utils' --without-sandbox
+    nosetests --gae-lib-root="$1" --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' --where='./reports/tests' --without-sandbox # --with-coverage  
+    nosetests --gae-lib-root="$1" --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' --where='./account/tests' --without-sandbox
+              --gae-lib-root="$1" 
+    # System T--gae-lib-root="$1" ests (Must live in server root dir for some reason TODO: Fix this)
+    nosetests --gae-lib-root="$1" --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' system_mptests
+    nosetests --gae-lib-root="$1" --match='(?:^|[\b_\./-])mptest' --with-gae --gae-application='./' network_config_mptests
+    nosetests --gae-lib-root="$1" --with-gae --gae-datastore='./test_data/basic_test.datastore' ad_server_tests # --with-coverage
 fi
 
 

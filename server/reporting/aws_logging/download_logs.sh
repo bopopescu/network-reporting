bf244ccdee6662d170f523752051e42490109850
James Sun <james@mopub.com>
2011-05-24 17:22:16 -0700

    add debug stuff to shell script

--- server/reporting/aws_logging/download_logs.sh
+++ server/reporting/aws_logging/download_logs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 PATH=.:$PATH:/usr/local/bin:~/google_appengine
+echo $PATH
+pwd
+
 
 APP_DIR=~/mopub/server
 LOG_ROOT_DIR=~/aws_logs 
@@ -9,7 +12,7 @@ mkdir $LOG_ROOT_DIR
 # download logs from GAE
 START_TIME=$(date +%s)
 echo
-echo N47935 | ./appcfg.py --no_cookies --email=olp@mopub.com --passin --append --num_days=1 --verbose request_logs $APP_DIR $LOG_ROOT_DIR/request-logfile
+echo N47935 | appcfg.py --no_cookies --email=olp@mopub.com --passin --append --num_days=1 --verbose request_logs $APP_DIR $LOG_ROOT_DIR/request-logfile
 #echo N47935 | appcfg.py --no_cookies --email=olp@mopub.com --passin --num_days=3 request_logs $APP_DIR $LOG_ROOT_DIR/request-logfile
 echo
 STOP_TIME=$(date +%s)

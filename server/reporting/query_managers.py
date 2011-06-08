930d53869e130da36bf87ed1305584f2c19118de
Nick Grabenstein <nick@mopub.com>
2011-06-08 16:59:47 -0700

    Query manager now with epic lambdas and maps

--- server/reporting/query_managers.py
+++ server/reporting/query_managers.py
@@ -51,8 +51,9 @@ class BlobLogQueryManager():
         return
 
     def get_blobkeys_for_days(days): 
-        #lambda magixxxx
-        return map(lambda day: BLOBLOG_KEY % day.strftime('%y%m%d'), days)
+        #for all the days, turn them into YYMMDD and then use that to construct the key, then with all those keys get all the BlobLogs, then with all those bloblogs, return only a list of the blob_keys associated with them
+        #this comment is longer than the code lul
+        return map(lambda bloblog: bloblog.blob_key, BlobLog.get(map(lambda day: db.Key(BLOBLOG_KEY % day.strftime('%y%m%d')), days)))
         
 class StatsModelQueryManager(CachedQueryManager):
     Model = StatsModel

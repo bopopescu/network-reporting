86ea430a5d5605f637591d7db6ad2267575a1966
nottombrown <nottombrown@gmail.com>
2011-06-09 19:25:42 -0700

    caching works

--- server/common/utils/decorators.py
+++ server/common/utils/decorators.py
@@ -94,7 +94,7 @@ def cache_page_until_post(time=5*60):
                 # If we are not POSTing, add to cache
                 try:
                     # Do the standard caching and vary on cookie
-                    vary_on_cookie_view = vary_on_headers('USER_ID')
+                    vary_on_cookie_view = vary_on_headers('USER_ID')(view)
                     cached_view = cache_page(time)(vary_on_cookie_view)
                     
                     return cached_view(request, *args, **kw)

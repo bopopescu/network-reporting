21dfcf0d553198717add85eb2af105474cf2a30d
Nafis Jamal <nafisjamal@mopub.com>
2011-06-23 16:43:11 -0700

    exposing conversion tracking in UI

--- server/advertiser/forms.py
+++ server/advertiser/forms.py
@@ -165,7 +165,7 @@ class AdGroupForm(mpforms.MPModelForm):
 class AbstractCreativeForm(mpforms.MPModelForm):
     def save(self,commit=True):
         obj = super(AbstractCreativeForm,self).save(commit=False)  
-        if obj.url:
+        if not obj.conv_appid and obj.url:
             obj.conv_appid = self._get_appid(obj.url)
             
         if commit:
@@ -209,7 +209,7 @@ class BaseCreativeForm(AbstractCreativeForm):
 
     class Meta:
         model = Creative
-        fields = ('ad_type','name','tracking_url','url','display_url','format','custom_height','custom_width','landscape')
+        fields = ('ad_type','name','tracking_url','url','display_url','format','custom_height','custom_width','landscape', 'conv_appid')
                     
 class TextCreativeForm(AbstractCreativeForm):
     TEMPLATE = 'advertiser/forms/text_creative_form.html'
@@ -217,7 +217,7 @@ class TextCreativeForm(AbstractCreativeForm):
     class Meta:
         model = TextCreative
         fields = ('headline','line1','line2') + \
-                 ('ad_type','name','tracking_url','url','display_url','format','custom_height','custom_width','landscape')
+                 ('ad_type','name','tracking_url','url','display_url','format','custom_height','custom_width','landscape', 'conv_appid')
         
 class TextAndTileCreativeForm(AbstractCreativeForm):
     TEMPLATE = 'advertiser/forms/text_tile_creative_form.html'
@@ -227,7 +227,7 @@ class TextAndTileCreativeForm(AbstractCreativeForm):
     
     class Meta:
         model = TextAndTileCreative
-        fields = ('line1','line2', 'ad_type','name','tracking_url','url','format','custom_height','custom_width','landscape')
+        fields = ('line1','line2', 'ad_type','name','tracking_url','url','format','custom_height','custom_width','landscape', 'conv_appid')
       
     def __init__(self, *args,**kwargs):
         instance = kwargs.get('instance',None)
@@ -259,7 +259,7 @@ class HtmlCreativeForm(AbstractCreativeForm):
     class Meta:
         model = HtmlCreative
         fields = ('html_data',) + \
-                 ('ad_type','name','tracking_url','url','display_url','format','custom_height','custom_width','landscape')
+                 ('ad_type','name','tracking_url','url','display_url','format','custom_height','custom_width','landscape', 'conv_appid')
                
 class ImageCreativeForm(AbstractCreativeForm):
     TEMPLATE = 'advertiser/forms/image_creative_form.html'
@@ -269,7 +269,7 @@ class ImageCreativeForm(AbstractCreativeForm):
     
     class Meta:
         model = ImageCreative
-        fields = ('ad_type','name','tracking_url','url','display_url','format','custom_height','custom_width','landscape') 
+        fields = ('ad_type','name','tracking_url','url','display_url','format','custom_height','custom_width','landscape', 'conv_appid') 
         
     def __init__(self, *args,**kwargs):
         instance = kwargs.get('instance',None)

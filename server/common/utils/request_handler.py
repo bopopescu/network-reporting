4d37bacfc86c3536ac00ec26bf898b8c04f02869
Nick Grabenstein <nick@mopub.com>
2011-07-21 12:34:12 -0700

    FINE NAFIS GOSH

--- server/common/utils/request_handler.py
+++ server/common/utils/request_handler.py
@@ -81,12 +81,13 @@ class RequestHandler(object):
             elif request.method == "POST":
                 # Now we can define get/post methods with variables instead of having to get it from the 
                 # Query dict every time! hooray!
-                audit_logger.log(simplejson.dumps({"user_email": self.request.user.email if self.request.user.is_authenticated() else 'Anonymous@nowhere.com', 
-                                                   "account_email": self.account.mpuser.email,
-                                                   "account_key": str(self.account.key()),
-                                                   "time": datetime.now(Pacific_tzinfo()).isoformat(), 
-                                                   "url": self.request.get_full_path(),
-                                                   "body": request.POST}))
+                if self.request.user.is_authenticated():
+                    audit_logger.log(simplejson.dumps({"user_email": self.request.user.email, 
+                                                       "account_email": self.account.mpuser.email,
+                                                       "account_key": str(self.account.key()),
+                                                       "time": datetime.now(Pacific_tzinfo()).isoformat(), 
+                                                       "url": self.request.get_full_path(),
+                                                       "body": request.POST}))
                 f_args = getargspec(self.post)[0]
                 for arg in f_args:
                     if not kwargs.has_key(arg) and self.params.has_key(arg):

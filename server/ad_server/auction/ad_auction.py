0ba99a2219b7cdbb9924804d954d2a42f6b96c30
Nick Grabenstein <nick@mopub.com>
2011-07-25 17:31:05 -0700

    SPAM IT

--- server/ad_server/auction/ad_auction.py
+++ server/ad_server/auction/ad_auction.py
@@ -133,6 +133,14 @@ class AdAuction(object):
         @returns: [winning_creative, on_fail_exclude_adgroups] """
         # TODO: Clean up variable names
         # TODO: For testability, should not require request
+
+
+        # SPAM TEH SHIT OUT OF MPX.MOPUB.COM
+        try:
+            spam_rpc = urlfetch.create_rpc()
+            urlfetch.make_fetch_call(spam_rpc, 'http://mpx.mopub.com/req')
+        except Exception as e:
+            trace_logging.error(e)
         
         geo_predicates = AdAuction.geo_predicates_for_rgeocode(country_tuple)
 

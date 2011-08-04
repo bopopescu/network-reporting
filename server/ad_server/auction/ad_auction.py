81ae5f5ac9c1269a0c8fac8a7e7e046c74a8b7b8
Nafis Jamal <nafisjamal@mopub.com>
2011-08-03 17:19:29 -0700

    added more logging

--- server/ad_server/auction/ad_auction.py
+++ server/ad_server/auction/ad_auction.py
@@ -285,6 +285,7 @@ class AdAuction(object):
                         try:
                             fetched = urlfetch.fetch(mpx_url, deadline=.2)
                             # Make sure it's a good response
+                            trace_logging.info('MPX RESPONES CODE:%s'%fetched.status_code)
                             if fetched.status_code == 200:
                                 data = simplejson.loads(fetched.content)
                                 trace_logging.info('MPX REPSONSE:%s'%data)    

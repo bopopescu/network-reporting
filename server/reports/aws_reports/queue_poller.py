06a9ebaaa4b4ea02087021a019cf70c7957b3fc1
Ubuntu <ubuntu@ip-10-90-238-25.ec2.internal>
2011-08-18 18:30:04 +0000

    Custom logging, made init.d script, etc.

--- server/reports/aws_reports/queue_poller.py
+++ server/reports/aws_reports/queue_poller.py
@@ -1,4 +1,6 @@
+#!/usr/bin/python
 import time, sys, os, time, urllib2
+from datetime import datetime
 
 sys.path.append("..")
 sys.path.append("../..")
@@ -13,26 +15,32 @@ sys.path.append("/home/ubuntu/google_appengine/lib/antlr3")
 sys.path.append("/home/ubuntu/google_appengine/lib/django_1_2")
 sys.path.append("/home/ubuntu/google_appengine/lib/yaml/lib")
 
+my_log = open('/home/ubuntu/poller.log', 'a')
+
 from appengine_django import InstallAppengineHelperForDjango
 InstallAppengineHelperForDjango()
 
+############### Boto Imports ############### 
 from boto.emr.connection import EmrConnection
 from boto.sqs.connection import SQSConnection
 from boto.sqs.message import Message
 from boto.s3.connection import S3Connection as s3
 
+############### Poster Imports ############### 
 from poster.encode import multipart_encode
 from poster.streaminghttp import register_openers
 
-from reports.models import Report
-
+############### GAE Imports ############### 
 from google.appengine.ext import blobstore
 from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.ext.remote_api import remote_api_stub
 
-from report_mr_submitter import submit_job
+############### Mopub Imports ############### 
 from parse_utils import gen_report_fname, parse_msg
 from parse_utils import AWS_ACCESS_KEY, AWS_SECRET_KEY
+from report_mr_submitter import submit_job
+
+from reports.models import Report
 
 
 ################### S3 Stuff ######################
@@ -63,6 +71,11 @@ HOST = 'http://%s.%s.appspot.com' % (BACKEND, APP)
 URL_HANDLER_PATH = '/offline/get_upload_url'
 UPDATE_STATS_HANDLER_PATH = '/offline/update_stats'
 
+################## Constants ###################
+LOG_FORMAT = "%s:\t%s\n"
+
+def log(mesg):
+    my_log.write(LOG_FORMAT % (int(time.time()), mesg))
 
 def auth_func():
     return 'olp@mopub.com', 'N47935'
@@ -92,14 +105,22 @@ def upload_file(fd):
 
     upload_url_req = urllib2.Request(HOST + URL_HANDLER_PATH)
     upload_url = urllib2.urlopen(upload_url_req).read()
-    print
-    print "Upload url:"
-    print upload_url
 
     file_upload_req = urllib2.Request(upload_url, datagen, headers)
     blob_key = urllib2.urlopen(file_upload_req).read()
     return blob_key
-    
+
+
+def finalize_report(rep):
+    log("Finalizing %s" % rep)
+    report = Report.get(rep)
+    report.report_blob = blob_key
+    data = report.parse_report_blob(report.report_blob.open())
+    report.data = data
+    report.completed_at = datetime.now()
+    report.put()
+    report.notify_complete()
+    sys.exit(0)
 
 def notify_appengine(fname, msg):
     d1, d2, d3, start, end, rep, acct = parse_msg(msg)
@@ -115,16 +136,11 @@ def notify_appengine(fname, msg):
     blob_key = upload_file(f)
     # Fork this shit because I don't like to wait
     pid = os.fork()
+    # Fork is hot shit, look up what it does, but basically pid == True means parent, otherwise child
     if pid:
         return
     else:
-        report = Report.get(rep)
-        report.report_blob = blob_key
-        data = report.parse_report_blob(report.report_blob.open())
-        report.data = data
-        report.put()
-        print data
-        sys.exit(0)
+        finalize_report(rep)
     
 
 
@@ -137,21 +153,20 @@ def main_loop():
     while True:
         if report_queue.count() > 0:
             msgs = report_queue.get_messages(MAX_MSGS, visibility_timeout = 0)
-            print msgs
             for msg in msgs:
+                log("Processing %s" % msg.get_body())
                 if msg in to_del:
                     continue
                 # Start the MR job
-                print msg
                 job_id, fname = submit_job(*parse_msg(msg))
-                print job_id
                 if not fail_dict.has_key(msg.get_body()):
                     fail_dict[msg.get_body()] = 0
                 # Save the msg w/ the job id
                 job_msg_map[job_id] = (fname, msg)
-                print msg
                 # Remove the msg from the Q
                 to_del.append(msg)
+        else:
+            log("No Messages")
 
         for i in range(len(to_del)):
             msg = to_del.pop(0)
@@ -167,25 +182,23 @@ def main_loop():
                 time.sleep(15)
                 continue
             for job in statuses:
-               fname, msg = job_msg_map[str(job.jobflowid)]
-               if job_failed(job.state):
-                   fail_dict[msg.get_body()] += 1
-                   report_queue.write(msg)
-                   if fail_dict[msg.get_body()] <= 3:
-                       processed_jobs.append(job.jobflowid)
+                log("Job: %s in state: %s" % (job.jobid, job.state))
+                fname, msg = job_msg_map[str(job.jobflowid)]
+                if job_failed(job.state):
+                    fail_dict[msg.get_body()] += 1
+                    report_queue.write(msg)
+                    if fail_dict[msg.get_body()] <= 3:
+                        processed_jobs.append(job.jobflowid)
             # Notify GAE when a report is finished
-               elif job_succeeded(job.state):
-                   notify_appengine(fname, msg)
-                   processed_jobs.append(job.jobflowid)
+                elif job_succeeded(job.state):
+                    notify_appengine(fname, msg)
+                    processed_jobs.append(job.jobflowid)
 
 
 
         # Remove Completed/Failed jobs from the map so we don't
         # Repeatedly notify GAE//Continuously add a job that 
         # failed once
-        print to_del
-        print job_msg_map
-        print processed_jobs
         for job_id in processed_jobs:
             del(job_msg_map[job_id])
         time.sleep(10)

52ac660f5e465eb178341cb786c3fecd0af4a7d5
kayla <kayla@mopub.com>
2011-08-18 16:08:17 -0700

    stuff

--- server/network_scraping/jumptap_scraper.py
+++ server/network_scraping/jumptap_scraper.py
@@ -7,7 +7,10 @@ class NetworkScrapeRecord(object):
     pass
 
 def jumptap_scraper(network_credential, from_date, to_date):
-    some_dict ={"user": network_credential.username, "pass": network_credential.password, "fromDate": from_date, "toDate": to_date, "groupBy": "spot"}#,"sites": network_credential.app_name}
+    some_dict ={"user": network_credential.username, "pass": network_credential.password, "fromDate": from_date, "toDate": to_date, "groupBy": "spot"}
+    
+    if network_credential.app_name:
+        some_dict['sites'] = network_credential.app_name
     
     req = urllib2.Request('https://pa.jumptap.com/pa-2.0/pub-services/v10/report.html?'+urllib.urlencode(some_dict))
     response = urllib2.urlopen(req)
@@ -36,15 +39,16 @@ def jumptap_scraper(network_credential, from_date, to_date):
             nsr.adunit_name = vals[adunit_index]
             scrape_records['%s||%s'%(vals[app_index],vals[adunit_index])] = nsr
             
-    return scrape_records
-    # for key in scrape_records.keys():
-    #     print key
-    #     print scrape_records[key].impressions, ' ', scrape_records[key].clicks, ' ', scrape_records[key].net_revenue, ' ', scrape_records[key].requests, ' ', scrape_records[key].cpc, ' ', scrape_records[key].app_name, ' ', scrape_records[key].adunit_name
+    # return scrape_records
+    for key in scrape_records.keys():
+        print key
+        print scrape_records[key].impressions, ' ', scrape_records[key].clicks, ' ', scrape_records[key].net_revenue, ' ', scrape_records[key].requests, ' ', scrape_records[key].cpc, ' ', scrape_records[key].app_name, ' ', scrape_records[key].adunit_name
     
 if __name__ == '__main__':
     nc = NetworkConfidential()
     nc.username='vrubba'
     nc.password='fluik123!'
-    #nc.app_name='office jerk'
+    nc.app_name=None
+    #nc.app_name='pa_fluik_entertain_office_jerk_fre_drd_app'
     jumptap_scraper(nc, '7/1/2011','7/1/2011')
     
--- server/network_scraping/mobfox_scraper.py
+++ server/network_scraping/mobfox_scraper.py
@@ -1,49 +1,54 @@
 import urllib2
 import urllib
+import time
+import datetime
+
 class NetworkConfidential(object):
     pass
 
 class NetworkScrapeRecord(object):
     pass
 
-def jumptap_scraper(network_credential, from_date, to_date):
-    some_dict ={"api_key": network_credential.api_key, "publisher_id": network_credential.publisher_id, "start_date": from_date, "end_date": to_date}#,"sites": network_credential.app_name}
+def mobfox_scraper(network_credential, from_date, to_date):
+    some_dict ={"api_key": network_credential.api_key, "publisher_id": network_credential.publisher_id, "start_date": from_date, "end_date": to_date}
     
-    req = urllib2.Request('http://account.mobfox.com/reporLng_api.php'+urllib.urlencode(some_dict))
+    req = urllib2.Request('http://account.mobfox.com/reporting_api.php?'+urllib.urlencode(some_dict))
     response = urllib2.urlopen(req)
-    
-    headers = response.readline().split(',')
-    
-    imp_index = headers.index('Paid Impressions')
-    click_index = headers.index('Clicks')
-    net_rev_index = headers.index('Net Revenue$')
-    requests_index = headers.index('Requests')
-    cpc_index = headers.index('Net Cost Per Click')
-    app_index = headers.index('Site')
-    adunit_index = headers.index('Spot')
-    
-    scrape_records = {}
+
     for line in response:
-        vals = line.split(',')
-        if vals[0] != 'Totals':
-            nsr = NetworkScrapeRecord()
-            nsr.impressions = vals[imp_index]
-            nsr.clicks = vals[click_index]
-            nsr.net_revenue = vals[net_rev_index]
-            nsr.requests = vals[requests_index]
-            nsr.cpc = vals[cpc_index]
-            nsr.app_name = vals[app_index]
-            nsr.adunit_name = vals[adunit_index]
-            scrape_records['%s||%s'%(vals[app_index],vals[adunit_index])] = nsr
-            
-    return scrape_records
+        print line
+    # headers = response.readline().split(',')
+    # 
+    # imp_index = headers.index('Paid Impressions')
+    # click_index = headers.index('Clicks')
+    # net_rev_index = headers.index('Net Revenue$')
+    # requests_index = headers.index('Requests')
+    # cpc_index = headers.index('Net Cost Per Click')
+    # app_index = headers.index('Site')
+    # adunit_index = headers.index('Spot')
+    # 
+    # scrape_records = {}
+    # for line in response:
+    #     vals = line.split(',')
+    #     if vals[0] != 'Totals':
+    #         nsr = NetworkScrapeRecord()
+    #         nsr.impressions = vals[imp_index]
+    #         nsr.clicks = vals[click_index]
+    #         nsr.net_revenue = vals[net_rev_index]
+    #         nsr.requests = vals[requests_index]
+    #         nsr.cpc = vals[cpc_index]
+    #         nsr.app_name = vals[app_index]
+    #         nsr.adunit_name = vals[adunit_index]
+    #         scrape_records['%s||%s'%(vals[app_index],vals[adunit_index])] = nsr
+    #         
+    # return scrape_records
+    
     # for key in scrape_records.keys():
     #     print key
     #     print scrape_records[key].impressions, ' ', scrape_records[key].clicks, ' ', scrape_records[key].net_revenue, ' ', scrape_records[key].requests, ' ', scrape_records[key].cpc, ' ', scrape_records[key].app_name, ' ', scrape_records[key].adunit_name
     
 if __name__ == '__main__':
     nc = NetworkConfidential()
-    nc.api_key='??'
-    nc.publisher_id='??'
-    #nc.app_name='office jerk'
-    jumptap_scraper(nc, '7/1/2011','7/1/2011')
+    nc.api_key='e64d04079c63c14644fc9690a925c8af'
+    nc.publisher_id='fb8b314d6e62912617e81e0f7078b47e'
+    mobfox_scraper(nc, time.mktime(datetime.date(2011,8,10).timetuple()),time.mktime(datetime.date(2011,8,12).timetuple()))

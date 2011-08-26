64c444154e61ca7d86be0ee35b7992991d741772
John Pena <john@mopub.com>
2011-08-25 21:09:15 -0700

    making the payments change form. correct submitting still doesnt work

--- server/account/forms.py
+++ server/account/forms.py
@@ -1,15 +1,16 @@
 from django import forms
 from common.utils import forms as mpforms
 from common.utils import fields as mpfields
-from account.models import Account, NetworkConfig
+from common.utils import widgets as mpwidgets
+from account.models import Account, NetworkConfig, PaymentInfo
 from common.constants import (ISO_COUNTRIES, US_STATES)
 
 class AccountForm(mpforms.MPModelForm):
     TEMPLATE = 'account/form/account_form.html'
     countries = ISO_COUNTRIES
     states = US_STATES
-    
-    phone = mpfields.MPTextField(label="Phone #", required=False)    
+
+    phone = mpfields.MPTextField(label="Phone #", required=False)
     class Meta:
         model = Account
         fields = ("admob_pub_id",
@@ -24,15 +25,37 @@ class AccountForm(mpforms.MPModelForm):
                   "jumptap_pub_id",
                   "millenial_pub_id",
                   "mobfox_pub_id",
-                  )                        
-        
+                  )
+
 class NetworkConfigForm(mpforms.MPModelForm):
-    
+
     class Meta:
         model = NetworkConfig
-    
+
     def clean(self):
         cleaned_data = self.cleaned_data
         for key, value in cleaned_data.iteritems():
             cleaned_data[key] = value.strip() if value else value
         return cleaned_data
+
+
+class PaymentInfoForm(mpforms.MPModelForm):
+    TEMPLATE = "account/form/paymentinfo_form.html"
+    country = mpfields.MPChoiceField(required=True, widget=mpwidgets.MPSelectWidget, choices=ISO_COUNTRIES)
+    payment_preference = mpfields.MPChoiceField(required=True,
+                                                widget=mpwidgets.MPRadioWidget,
+                                                choices=(('paypal', 'Paypal'), ('wire', 'Wire Transfer')))
+
+    class Meta:
+        model = PaymentInfo
+        fields = ('country',
+                  'payment_preference',
+                  'us_tax_id',
+                  'local_tax_id',
+                  'paypal_email,',
+                  'beneficiary_name',
+                  'bank_name',
+                  'bank_address',
+                  'account_number',
+                  'ach_routing_number',
+                  'bank_swift_code',)
--- server/account/views.py
+++ server/account/views.py
@@ -8,7 +8,7 @@ from common.ragendja.template import render_to_response
 from common.utils.query_managers import CachedQueryManager
 
 from account.models import Account
-from account.forms import AccountForm, NetworkConfigForm
+from account.forms import AccountForm, NetworkConfigForm, PaymentInfoForm
 from account.query_managers import AccountQueryManager
 from publisher.query_managers import AdUnitQueryManager, AdUnitContextQueryManager, AppQueryManager
 import logging
@@ -23,9 +23,12 @@ class AccountHandler(RequestHandler):
         account_form = account_form or AccountForm(instance=self.account)
         apps_for_account = AppQueryManager.get_apps(account=self.account)
         user = self.account.mpuser
-        
+
         networks = ['admob_status','adsense_status','brightroll_status','chartboost_status','ejam_status','greystripe_status','inmobi_status','jumptap_status','millennial_status','mobfox_status']
         network_config_status = {}
+
+        self.account.paymentinfo = self.account.payment_infos.get()
+
         def _get_net_status(account,network):
             status = 0
             # eg. account.admob_pub_id
@@ -48,12 +51,12 @@ class AccountHandler(RequestHandler):
                 return 4
             else:
                 return status
-            
+
         for network in networks:
             network_config_status[network] = _get_net_status(self.account,network)
-          
-      
-        return render_to_response(self.request,'account/account.html', dict({'account': self.account, 
+
+
+        return render_to_response(self.request,'account/account.html', dict({'account': self.account,
                                                                       'account_form': account_form,
                                                                       'user': user,
                                                                       "apps": apps_for_account}.items() + network_config_status.items()))
@@ -62,7 +65,7 @@ class AccountHandler(RequestHandler):
     def post(self):
         account_form = AccountForm(data=self.request.POST, instance=self.account)
         network_config_form = NetworkConfigForm(data=self.request.POST, instance=self.account.network_config)
-        
+
         if account_form.is_valid():
             account = account_form.save(commit=False)
             network_config = network_config_form.save(commit=False)
@@ -76,12 +79,12 @@ class AccountHandler(RequestHandler):
                     app_key_identifier = key.split('-__-')
                     if app_key_identifier[0] == str(app.key()):
                         app_network_config_data[app_key_identifier[1]] = value
-                
+
                 logging.warning("link" + unicode(app.name) + " " + str(app_network_config_data))
                 app_form = NetworkConfigForm(data=app_network_config_data, instance=app.network_config)
                 app_network_config = app_form.save(commit=False)
                 AppQueryManager.update_config_and_put(app, app_network_config)
-                
+
                 adunits = AdUnitQueryManager.get_adunits(app=app)
                 for adunit in adunits:
                     adunit_network_config_data = {}
@@ -92,16 +95,16 @@ class AccountHandler(RequestHandler):
                     adunit_form = NetworkConfigForm(data=adunit_network_config_data, instance=adunit.network_config)
                     adunit_network_config = adunit_form.save(commit=False)
                     AdUnitQueryManager.update_config_and_put(adunit, adunit_network_config)
-                            
-                
-                
+
+
+
             if self.account.status == "step3":
                 self.account.status = "step4"
                 AccountQueryManager.put_accounts(self.account)
                 return HttpResponseRedirect(reverse('advertiser_campaign'))
-        return self.get(account_form=account_form)        
+        return self.get(account_form=account_form)
 
-@login_required         
+@login_required
 def index(request,*args,**kwargs):
     return AccountHandler()(request,*args,**kwargs)
 
@@ -115,11 +118,11 @@ class NewAccountHandler(RequestHandler):
         # Make sure terms and conditions are agreed to
         if not self.request.POST.get("terms_conditions"):
             account_form.term_conditions_error = "Accept the terms and conditions in order to start using MoPub."
-            return self.get(account_form=account_form)    
+            return self.get(account_form=account_form)
 
         if account_form.is_valid():
             account = account_form.save(commit=False)
-            
+
             # Go ahead and activate the account
             account.active = True
             AccountQueryManager().put_accounts(account)
@@ -128,15 +131,35 @@ class NewAccountHandler(RequestHandler):
             return HttpResponseRedirect(reverse('publisher_app_create'))
 
         return self.get(account_form=account_form)
-        
+
 # We use login_required here since we want to let users activate themselves on this page
 @login_required
 def new(request,*args,**kwargs):
-    return NewAccountHandler()(request,*args,**kwargs)    
+    return NewAccountHandler()(request,*args,**kwargs)
 
 class LogoutHandler(RequestHandler):
     def get(self):
         return HttpResponseRedirect(users.create_logout_url('/main/'))
-        
+
+
+class PaymentInfoChangeHandler(RequestHandler):
+    def get(self, *args, **kwargs):
+        payment_info = self.account.payment_infos.get()
+        if self.request.method == 'POST':
+            form = PaymentInfoForm(self.request.POST, instance=payment_info)
+            if form.is_valid():
+                payment_info = form.save()
+                return redirect('account_index')
+        else:
+            form = PaymentInfoForm()
+
+        return render_to_response(self.request,
+                                  'account/payment_info_change.html',
+                                  {'form': form})
+
+@login_required
+def payment_info_change(request, *args, **kwargs):
+    return PaymentInfoChangeHandler()(request, *args, **kwargs)
+
 def logout(request,*args,**kwargs):
     return LogoutHandler()(request,*args,**kwargs)

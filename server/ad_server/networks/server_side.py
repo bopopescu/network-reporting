213b10f99813694e7217824bb7f3911cae87245e
nottombrown <nottombrown@gmail.com>
2011-08-30 13:18:11 -0700

    underscores

--- server/ad_server/networks/server_side.py
+++ server/ad_server/networks/server_side.py
@@ -57,7 +57,7 @@ class ServerSide(object):
     def bid_and_html_for_response(self, response):
         self.get_pub_id(warn=True) # get the pub id and warn if not present
         if response.status_code == 200:
-            response_tuple = list(self.bid_and_html_for_response(response))
+            response_tuple = list(self._bid_and_html_for_response(response))
             
             # Encode incoming text
             unencoded = response_tuple[1]
@@ -69,5 +69,5 @@ class ServerSide(object):
             trace_logging.info("Failed to load ad from %s"%self.network_name)    
             return None
         
-    def bid_and_html_for_response(self, response):
+    def _bid_and_html_for_response(self, response):
         return 0.0,"<html>BLAH</html>"     

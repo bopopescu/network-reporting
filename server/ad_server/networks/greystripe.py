2a880189a2658d80aa89bbe9c74691cde74b1a5e
nottombrown <nottombrown@gmail.com>
2011-09-02 15:51:06 -0700

    removed support for greystripe

--- server/ad_server/networks/greystripe.py
+++ server/ad_server/networks/greystripe.py
@@ -4,40 +4,7 @@ import urllib2
 from ad_server.debug_console import trace_logging
 from ad_server.networks.server_side import ServerSideException  
 class GreyStripeServerSide(ServerSide):    
-    """ Greystripe is being deprecated. """
-    base_url = "http://adsx.greystripe.com/openx/www/delivery/mw2.php"
-    pub_id_attr = 'greystripe_pub_id'
-    network_name = 'GreyStripe'
-    
-    
-    @property
-    def url(self):
-        return self.base_url
-      
-    @property
-    def headers(self):
-      # TODO: Replace with self.get_appid()
-        return {'User-Agent': self.client_context.user_agent}
+    """ Greystripe is deprecated. """
 
-    @property    
-    def payload(self):
-        data = {'language':'python',
-                'version':'1.0',
-                'format':'html',
-                'ip': self.client_context.client_ip,
-                'site_id': self.get_pub_id(),
-                }
-        return urllib.urlencode(data)
-    
-    def get_response(self):
-        req = urllib2.Request(self.url)
-        response = urllib2.urlopen(req)  
-        return response.read()
-      
-    def html_for_response(self, response):
-        if len(response.content) == 0 or \
-        response.status_code != 200 or \
-        """<script type='text/javascript'>/*<![CDATA[*/<a href='F' target='_blank'><img src='F' border='0' alt=''></a>/*]]>*/</script""" in response.content:
-            trace_logging.info("GreyStripe failed to return ad")
-            raise ServerSideException("GreyStripe ad is empty")  
-        return response.content
+    def make_call_and_get_html_from_response(self):
+        raise ServerSideException("Greystripe is deprecated")

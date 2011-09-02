8313dc68912efd534d38e77ab184578fd7675945
nottombrown <nottombrown@gmail.com>
2011-09-01 18:14:10 -0700

    added exception import

--- server/ad_server/networks/server_side.py
+++ server/ad_server/networks/server_side.py
@@ -3,7 +3,9 @@ import re
 from ad_server.debug_console import trace_logging
 from common.utils import helpers                    
 from common.utils.decorators import returns_unicode 
-from google.appengine.api import urlfetch
+from google.appengine.api import urlfetch  
+
+from google.appengine.runtime import DeadlineExceededError
 
 
 class ServerSide(object):

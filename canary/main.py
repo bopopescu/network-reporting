3c3117c1a18f540fb377cd1679d003260d06ceb0
tiago <tiago@mopub.com>
2011-09-09 12:39:16 -0700

    added template

--- canary/main.py
+++ canary/main.py
@@ -47,8 +47,8 @@ class PerformanceHandler(webapp.RequestHandler):
         last_success = memcache.get("last_success")
         avg_latency = memcache.get("avg_latency")
         
-        averages = [(d, m) for d, m, r in aggregate_requests]
-        last = [r for d, m, r in averages]
+        averages = [m for m, r in aggregate_requests]
+        last = [r for m, r in aggregate_requests]
 
         # determine overall health
         if averages and avg_latency:
@@ -65,15 +65,15 @@ class PerformanceHandler(webapp.RequestHandler):
         now = datetime.datetime.now()
 
         self.response.out.write(template.render('aggregatePerformance.html',
-           {"averages": averages[:50],
-            "last": last[:50], 
-            "error": error, "warning": warning, "ok": ok,
-            "now": now,
-            "start": now - datetime.timedelta(minutes=50),
-            "failure_rate": failure_rate, 
-            "last_success": last_success,         
-            "avg": avg_latency, 
-            "median": memcache.get("median_latency")}))
+                     {"averages": averages[:50],
+                      "last": last[:50], #memcache.get("agltb3B1Yi1pbmNyDAsSBFNpdGUYwLQgDA-last-requests")
+                      "error": error, "warning": warning, "ok": ok,
+                      "now": now,
+                      "start": now - datetime.timedelta(minutes=50),
+                      "failure_rate": failure_rate, 
+                      "last_success": last_success,         
+                      "avg": avg_latency, 
+                      "median": memcache.get("median_latency")}))
 
 ###
 # Shows a simple page that catalogs recent pings and the overall
@@ -115,42 +115,48 @@ class PerformanceIdHandler(webapp.RequestHandler):
 
 class PingHandler(webapp.RequestHandler):
     def get(self):
+        # ping all ad ids asynchronously
         rpcs = []
         ad_tests = AdTest.all()
         for ad_test in ad_tests:
-            logging.info("Pinging " + ad_test.ad_id)
             rpc = urlfetch.create_rpc()
             urlfetch.make_fetch_call(rpc, 'http://%s/ping/%s' % (self.request.host.replace('8080','8081'), ad_test.ad_id))
             rpcs.append(rpc)
         
+        # calculate average latency
         latency_sum = 0
+        latency_count = 0
         for rpc in rpcs:
-            latency_sum += json.loads(rpc.get_result())['request_ms']
-        aggregate_requests = memcache.get("aggregate_requests") or []
+            latency_sum += json.loads(rpc.get_result().content)['request_ms']
+            latency_count += 1
         
-        # pick a random request 
-        request_dict = json.loads(rpcs[int(random.random() * len(URLS))].get_result())
+        # pick a random request
+        request_dict = json.loads(rpcs[int(random.random() * len(rpcs))].get_result().content)
         now = datetime.datetime.now()
-        r = Request(host=request_dict['host'], urls=request_dict['url'], success=request_dict['success'], status_code=request_dict['status_code'],
-                    status_message=request_dict['status_message'], request_ms=request_dict['request_ms'], response_size=request_dict['response_size'],
-                    t=now)
-        aggregate_requests.insert(0, (now, latency_sum / len(ad_tests), r))
+        r = Request(host=request_dict['host'], url=request_dict['url'], success=request_dict['success'], status_code=request_dict['status_code'],
+        status_message=request_dict['status_message'], request_ms=request_dict['request_ms'], response_size=request_dict['response_size'],
+        t=now)
+        
+        aggregate_requests = memcache.get("aggregate_requests") or []
+        aggregate_requests.insert(0, (latency_sum / latency_count, r))
         memcache.set("aggregate_requests", aggregate_requests)
 
 ###
 # Pings ad server URL for the id
 
 class PingIdHandler(webapp.RequestHandler):
-	def get(self, id):
-	    r = Request(host="ads.mopub.com", url="/m/ad?v=3&id=" + id + URLS[int(random.random() * len(URLS))])
-	    r.go()
+    def get(self, id):
+        r = Request(host="ads.mopub.com", url="/m/ad?v=3&id=" + id + URLS[int(random.random() * len(URLS))])
+        r.go()
 
-	    # add this to memcache circular buffer
-	    id_last = memcache.get("%s-last-requests" % id) or []
-	    id_last.insert(0, r)
+        # add this to memcache circular buffer
+        id_last = memcache.get("%s-last-requests" % id) or []
+        id_last.insert(0, r)
         memcache.set("%s-last-requests" % id, id_last[:MAX_REQUESTS])
+        
+        # write out JSON response to return to PingHandler
         d = {'host': r.host, 'url': r.url, 'success': r.success, 'status_code': r.status_code, 'status_message': r.status_message,
-             'request_ms': r.request_ms, 'response_size': r.response_size}
+        'request_ms': r.request_ms, 'response_size': r.response_size}
         self.response.out.write(json.dumps(d))
 	    
 ###
@@ -165,33 +171,35 @@ class RecalculateHandler(webapp.RequestHandler):
         
         ad_tests = AdTest.all()
         for ad_test in ad_tests:
+            # recalculate statistics for all ad ids asynchronously
             rpc = urlfetch.create_rpc()
             urlfetch.make_fetch_call(rpc, 'http://%s/r/%s' % (self.request.host.replace('8080','8081'), ad_test.ad_id))
             
             id_last = memcache.get("%s-last-requests" % ad_test.ad_id) or []
 
-            # determines overall ad serving status
+            # update running totals for statistics
             failure_sum += sum(0 if x.success else 1 for x in id_last)
             request_count += len(id_last)
             id_last_successes = [i for i, v in enumerate(id_last) if v.success]
             if len(id_last_successes) > 0:
                 last_successes.append(min(id_last_successes))
-            
             latencies.extend([x.request_ms for x in id_last if x.request_ms is not None])
-            
-        failure_rate = failure_sum / float(request_count);
-        last_success = min(last_successes)
-        memcache.set("failure_rate", failure_rate)
-        memcache.set("last_success", last_success)
         
-        latencies.sort()
-        if len(latencies) > 0:
-            avg_latency = int(sum(latencies) / float(len(latencies)))
-            median_latency = latencies[len(latencies)/2] 
+        # determine aggregate ad serving status    
+        if request_count > 0:
+            failure_rate = failure_sum / float(request_count);
+            last_success = min(last_successes)
+            memcache.set("failure_rate", failure_rate)
+            memcache.set("last_success", last_success)
+        
+            latencies.sort()
+            if len(latencies) > 0:
+                avg_latency = int(sum(latencies) / float(len(latencies)))
+                median_latency = latencies[len(latencies)/2] 
 
-            # store latency stats in memcache
-            memcache.set("avg_latency", avg_latency)
-            memcache.set("median_latency", median_latency)
+                # store latency stats in memcache
+                memcache.set("avg_latency", avg_latency)
+                memcache.set("median_latency", median_latency)
         
 ###
 # Recalculates fun statistics and potentially sends canary emails for an ad id
@@ -225,7 +233,9 @@ class RecalculateIdHandler(webapp.RequestHandler):
             memcache.set("%s-avg_latency" % id, avg_latency)
             memcache.set("%s-median_latency" % id, median_latency)
 
-application = webapp.WSGIApplication([('/performance', PerformanceHandler), # shows a splash page containing aggregate performance data
+application = webapp.WSGIApplication([
+                  ('/', PerformanceHandler),
+                  ('/performance', PerformanceHandler), # shows a splash page containing aggregate performance data
                   ('/performance/([A-Za-z0-9]*)', PerformanceIdHandler),    # shows a splash page containing performance data for ad id
 				  ('/ping', PingHandler),                                   # pings all ad ids
                   ('/ping/([A-Za-z0-9]*)', PingIdHandler),                  # pings ad server URL for ad id

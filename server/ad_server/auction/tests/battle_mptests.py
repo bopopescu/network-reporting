639b6e6f8d33603b31cbf7d3a70e0eb6e64cf165
Nafis Jamal <nafisjamal@mopub.com>
2011-09-27 16:16:54 -0700

    fixed revenue in test

--- server/ad_server/auction/tests/battle_mptests.py
+++ server/ad_server/auction/tests/battle_mptests.py
@@ -387,7 +387,7 @@ class TestAdAuction(unittest.TestCase):
         self.adunit_context = AdUnitContextQueryManager.cache_get_or_insert(self.adunit_id) 
 
         test_html = '<html>blah....</html>'
-        content = '{"xhtml_real": "%s", "pub_rev": 10000.50}' % test_html
+        content = '{"xhtml_real": "%s", "revenue": 10000.50}' % test_html
         marketplace_battle = MarketplaceBattle(self.client_context, self.adunit_context)
         creative = marketplace_battle._process_marketplace_response(content, self.dummy_marketplace_creative)
         #creative = marketplace_battle.run() 

eb0bc744fafe556e4116e4790dee674f96a55ed0
Tiago Bandeira <tiago@mopub.com>
2011-10-10 11:48:47 -0700

    Better comments + minor formatting changes for network scraping

--- server/network_scraping/query_managers.py
+++ server/network_scraping/query_managers.py
@@ -14,22 +14,28 @@ class Stats(object):
 class AdNetworkReportQueryManager(CachedQueryManager):
 
     def __init__(self, account=None):
-        if isinstance(account, db.Key):
-            self.account = account
-        elif isinstance(account, db.Model):
-            self.account = account.key()
-        elif account is None:
-            self.account = None
-        else:
-            self.account = Account.get_by_key_name(account) #db.Key(account) 
+        self.account = account
+        # if isinstance(account, db.Key):
+        #     self.account = account
+        # elif isinstance(account, db.Model):
+        #     self.account = account.key()
+        # elif account is None:
+        #     self.account = None
+        # else:
+        #     self.account = Account.get_by_key_name(account) #db.Key(account) 
         
     def get_ad_network_mappers(self):
-        """ Get the aggregate stats for the different apps given an account """
+        """Inner join AdNetworkLoginInfo with AdNetworkAppMapper.
+        
+        Return a generator of the AdNetworkAppMappers with this account."""
         for l in AdNetworkLoginInfo.all().filter('account =', self.account):
             for n in AdNetworkAppMapper.all().filter('ad_network_login =', l):
                 yield n
                 
     def get_ad_network_aggregates(self, ad_network_app_mapper, start_date, end_date):
+        """Calculate aggregate stats for an ad network and app between the given start date and end date.
+        
+        Return the aggregate stats."""
         q = AdNetworkScrapeStats.all()
         q.filter('ad_network_app_mapper =', ad_network_app_mapper)
         q.filter('date IN', date_magic.gen_days(start_date, end_date))
@@ -65,18 +71,20 @@ class AdNetworkReportQueryManager(CachedQueryManager):
         return aggregate_stats
 
     def get_ad_network_app_stats(self, ad_network_app_mapper):
-        """ Get the AdNetworkStats for a given ad_network_app_mapper sorted
-        chronologically by day, newest first (decending order) """
-        q = AdNetworkScrapeStats.all()
-        q.filter('ad_network_app_mapper =', ad_network_app_mapper)
-        q.order('-date')
-        return q
+        """Filter AdNetworkStats for a given ad_network_app_mapper. Sort chronologically by day, newest first (decending order.)
         
-    # (ad_network_app_mapper_key) or (publisher_id, login_info)
+        Return the query (a generator.)
+        """
+        query = AdNetworkScrapeStats.all()
+        query.filter('ad_network_app_mapper =', ad_network_app_mapper)
+        query.order('-date')
+        return query
+        
     def get_ad_network_app_mapper(self, ad_network_app_mapper_key = None, publisher_id = None, login_info = None):
-        """ Get the AdNetworkAppMapper for a given publisher id and 
-        login info """
+        """Keyword arguments: either an ad_network_app_mapper_key or a publisher_id and login_info.
         
+        Return the corresponding AdNetworkAppMapper.
+        """
         if ad_network_app_mapper_key:
             return db.get(ad_network_app_mapper_key)
         elif publisher_id and login_info:
@@ -87,10 +95,26 @@ class AdNetworkReportQueryManager(CachedQueryManager):
         return None
         
     def get_apps_with_publisher_ids(self, ad_network_name):
-        """ Get generator of applications with publisher ids for the account on the ad_network """
+        """Inner join of NetworkConfig and App.
+        
+        Return generator of applications with publisher ids for the account on the ad_network.
+        """
         for n in NetworkConfig.all().filter('%s_pub_id !=' % ad_network_name, None):
             for a in App.all().filter('account =', self.account).filter('network_config =', n):
                 yield (a, eval('n.%s_pub_id' % ad_network_name))
+                
+    def create_login_info_and_mappers(self, ad_network_name, username, password, client_key, send_email):
+        """Create AdNetworkLoginInfo and AdNetworkAppMapper entities."""
+        apps_with_publisher_ids = list(self.get_apps_with_publisher_ids(ad_network_name))
+        publisher_ids = [publisher_id for app, publisher_id in apps_with_publisher_ids]
+        
+        login_info = AdNetworkLoginInfo(account = self.account, ad_network_name = ad_network_name, username = username, password = password, client_key = client_key, publisher_ids = publisher_ids)
+        login_info.put()
+        
+        # Create all the different AdNetworkAppMappers for all the applications on the ad network for the user and add them to the db
+        db.put([AdNetworkAppMapper(ad_network_name = ad_network_name, publisher_id = publisher_id,
+                ad_network_login = login_info, application = app, send_email = send_email) for 
+                app, publisher_id in apps_with_publisher_ids])
         
 def get_pub_id(pub_id, login_info):
     return pub_id
--- server/network_scraping/views.py
+++ server/network_scraping/views.py
@@ -8,24 +8,14 @@ from common.utils.request_handler import RequestHandler
 from datetime import timedelta, date
 from network_scraping.query_managers import AdNetworkReportQueryManager
 
-from google.appengine.ext import db
-from network_scraping.models import *
-
-# from network_scraping import ad_networks
-
-IS_PRODUCTION = False
-
 class AdNetworkReportIndexHandler(RequestHandler):
     def get(self):
-        if IS_PRODUCTION:
-            manager = AdNetworkReportQueryManager(self.account)
-            mappers = manager.get_ad_network_mappers()
-        else:
-            # from network_scraping.load_test_data import TestDataLoader
-            # manager = AdNetworkReportQueryManager(TestDataLoader.ACCOUNT_KEY_NAME)
-            manager = AdNetworkReportQueryManager('test')
-            query = AdNetworkAppMapper.all()
-            mappers = list(query)
+        """Get a list of aggregtate stats for the ad networks, apps and account.
+        
+        Return a webpage with the list of stats in a table.
+        """
+        manager = AdNetworkReportQueryManager(self.account)
+        mappers = list(manager.get_ad_network_mappers())
         
         keys = [s.key() for s in mappers]
         # Get aggregate stats for all the different ad network mappers for the account between the selected date range
@@ -44,9 +34,11 @@ def ad_network_report_index(request, *args, **kwargs):
      
 class ViewAdNetworkReportHandler(RequestHandler):
     def get(self, ad_network_app_mapper_key, *args, **kwargs):
-        manager = AdNetworkReportQueryManager(self.account)
+        """Get a list of stats for the ad network, app and account.
         
-        logging.warning("ad_network_app_mapper_key = %s" % ad_network_app_mapper_key)
+        Return a webpage with the list of stats in a table.
+        """
+        manager = AdNetworkReportQueryManager(self.account)
         ad_network_app_mapper = manager.get_ad_network_app_mapper(ad_network_app_mapper_key = ad_network_app_mapper_key)
         dates = manager.get_ad_network_app_stats(ad_network_app_mapper)
         return render_to_response(self.request, 'network_scraping/view_app_ad_network_report.html',
@@ -59,7 +51,7 @@ def view_ad_network_app_report(request, *args, **kwargs):
     
 class AddLoginInfoHandler(RequestHandler):
     def get(self): #Verfify that this is SSL
-        """ Input login info and select what apps you want to use it for and store it in the db """
+        """Return form with ad network login info."""
         from account.models import NetworkConfig
         from publisher.models import App
         
@@ -72,8 +64,11 @@ class AddLoginInfoHandler(RequestHandler):
         return render_to_response(self.request, 'network_scraping/add_login_info.html',
                                   dict(ad_network_names = ['admob', 'jumptap', 'iad', 'inmobi', 'mobfox']))#ad_networks.ad_networks.keys()))
                                   
-    def post(self): #, ad_network_name, username = None, password = None, client_key = None, send_email = False
-        """ Create AdNetworkLoginInfo and AdNetworkAppMappers for all apps that have pub ids for this network and account """
+    def post(self):
+        """Create AdNetworkLoginInfo and AdNetworkAppMappers for all apps that have pub ids for this network and account.
+        
+        Return a redirect to the ad nework report index.
+        """
         ad_network_name = self.request.POST['ad_network_name']
         username = self.request.POST['username']
         password = self.request.POST['password']
@@ -81,20 +76,7 @@ class AddLoginInfoHandler(RequestHandler):
         send_email = self.request.POST.get('send_email', False)
         
         manager = AdNetworkReportQueryManager(self.account)
-        
-        
-        apps_with_publisher_ids = list(manager.get_apps_with_publisher_ids(ad_network_name))
-        
-        # get the apps in the ad network
-        publisher_ids = [publisher_id for app, publisher_id in apps_with_publisher_ids]
-        
-        login_info = AdNetworkLoginInfo(account = self.account, ad_network_name = ad_network_name, username = username, client_key = client_key, publisher_ids = publisher_ids)
-        login_info.put()
-        
-        # Create all the different AdNetworkAppMappers for all the applications on the ad network for the user and add them to the db
-        db.put([AdNetworkAppMapper(ad_network_name = ad_network_name, publisher_id = publisher_id,
-                ad_network_login = login_info, application = app, send_email = False) for 
-                app, publisher_id in apps_with_publisher_ids])
+        manager.create_login_info_and_mappers(ad_network_name, username, password, client_key, send_email)
                 
         return redirect('ad_network_reports_index')
         

203b6b6ccc4b4d4cf9fc96030e0cc3b0ccfae2b6
Bryan Atwood <batwood@mopub.com>
2011-10-21 12:39:14 -0700

    Custom email for reporting
    Fix boolean email flag error, only send one email
    
    Fix a couple of small errors

--- server/mail/mails.py
+++ server/mail/mails.py
@@ -1,8 +1,8 @@
 REPORT_FINISHED_SIMPLE = \
-"""Your report has finished running.  You can view it at:
+"""Your report for %(dim1)s - %(dim2)s - %(dim3)s ranging from dates %(start)s to %(end)s has finished running.  You can view it at:
 
     http://app.mopub.com/reports/view/%(report_key)s/
 """
 
 REPORT_FAILED_SIMPLE = \
-""" Your report for %(dim1)s - %(dim2)s - %(dim3)s ranging from dates %(start)s to %(end)s has failed.  This could either be due to your account not having any data for the days specified or other transient errors.  Please try again, or if you are receiving multiple failure notices please notify support@mopub.com""" 
+""" Your report for %(dim1)s - %(dim2)s - %(dim3)s ranging from dates %(start)s to %(end)s has failed.  This could either be due to your account not having any data for the days specified or other transient errors.  Please try again, or if you are receiving multiple failure notices please notify support@mopub.com"""

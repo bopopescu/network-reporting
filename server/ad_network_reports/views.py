351707ddf87d7cc57c46d42b292f92e0f9d9d11e
Tiago Bandeira <tiago@mopub.com>
2011-10-24 15:14:47 -0700

    Enhancements to the front-end

--- server/ad_network_reports/views.py
+++ server/ad_network_reports/views.py
@@ -1,5 +1,6 @@
 import logging
 
+from ad_network_reports.forms import LoginInfoForm
 from ad_network_reports.query_managers import AdNetworkReportQueryManager, \
         create_manager
 from common.ragendja.template import render_to_response
@@ -33,8 +34,10 @@ class AdNetworkReportIndexHandler(RequestHandler):
                 s[1].application.name + s[1].ad_network_name)
 
         return render_to_response(self.request,
-                'ad_network_reports/ad_network_index.html', dict(aggregate_stats
-                    = aggregate_stats))
+                                  'ad_network_reports/ad_network_index.html',
+                                  {
+                                      "aggregate_stats" : aggregate_stats,
+                                  })
 
 @login_required
 def ad_network_report_index(request, *args, **kwargs):
@@ -51,10 +54,15 @@ class ViewAdNetworkReportHandler(RequestHandler):
                 ad_network_app_mapper_key = ad_network_app_mapper_key)
         dates = manager.get_ad_network_app_stats(ad_network_app_mapper)
         return render_to_response(self.request,
-                'ad_network_reports/view_app_ad_network_report.html',
-                dict(ad_network_name = ad_network_app_mapper.ad_network_name,
-                     app_name = ad_network_app_mapper.application.name,
-                     dates = dates))
+                                  'ad_network_reports/'
+                                  'view_app_ad_network_report.html',
+                                  {
+                                      "ad_network_name" :
+                                      ad_network_app_mapper.ad_network_name,
+                                      "app_name" : ad_network_app_mapper.
+                                      application.name,
+                                       "dates" : dates
+                                  })
 
 @login_required
 def view_ad_network_app_report(request, *args, **kwargs):
@@ -96,11 +104,29 @@ class AddLoginInfoHandler(RequestHandler):
 #
 #        officejerk_app = App(account = self.account, name = "Office Jerk", network_config = officejerk_network_config)
 #        officejerk_app.put()
+        if account_key:
+            account = Account.get(account)
+        else:
+            account = self.account
+
+        forms = []
+        for name in AD_NETWORK_NAMES:
+            try:
+                instance = AdNetworkLoginInfo.get_by_network(account, name)
+                form = LoginInfoForm(instance=instance, prefix=name)
+            except Exception, error:
+                instance = None
+                form = LoginInfoForm(prefix=name)
+            form.ad_network = name
+            forms.append(form)
 
         return render_to_response(self.request,
-                'ad_network_reports/add_login_info.html',
-                dict(ad_network_names=AD_NETWORK_NAMES,
-                    account_key=account_key, error=""))
+                                  'ad_network_reports/add_login_info.html',
+                                  {
+                                      'ad_network_names' : AD_NETWORK_NAMES,
+                                      'forms' : forms,
+                                      'error' : "",
+                                  })
 
     def post(self, account_key=None):
         """Create AdNetworkLoginInfo and AdNetworkAppMappers for all apps that

a8636c0c3781c14cede0a1fb28a608fdb71ca66b
Bryan Atwood <batwood@mopub.com>
2011-10-26 14:56:08 -0700

    Change sender of welcome email to MoPub, Inc. <olp@mopub.com>

--- server/registration/views.py
+++ server/registration/views.py
@@ -23,45 +23,45 @@ def activate(request, activation_key,
     """
     Activate a ``User``'s account from an activation key, if their key
     is valid and hasn't expired.
-    
+
     By default, use the template ``registration/activate.html``; to
     change this, pass the name of a template as the keyword argument
     ``template_name``.
-    
+
     **Required arguments**
-    
+
     ``activation_key``
        The activation key to validate and use for activating the
        ``User``.
-    
+
     **Optional arguments**
-       
+
     ``extra_context``
         A dictionary of variables to add to the template context. Any
         callable object in this dictionary will be called to produce
         the end result which appears in the context.
-    
+
     ``template_name``
         A custom template to use.
-    
+
     **Context:**
-    
+
     ``account``
         The ``User`` object corresponding to the account, if the
         activation was successful. ``False`` if the activation was not
         successful.
-    
+
     ``expiration_days``
         The number of days for which activation keys stay valid after
         registration.
-    
+
     Any extra variables supplied in the ``extra_context`` argument
     (see above).
-    
+
     **Template:**
-    
+
     registration/activate.html or ``template_name`` keyword argument.
-    
+
     """
     activation_key = activation_key.lower() # Normalize before trying anything with it.
     account = RegistrationProfile.objects.activate_user(activation_key)
@@ -81,67 +81,67 @@ def register(request, success_url=None,
              extra_context=None, auto_login=True):
     """
     Allow a new user to register an account.
-    
+
     Following successful registration, issue a redirect; by default,
     this will be whatever URL corresponds to the named URL pattern
     ``registration_complete``, which will be
     ``/accounts/register/complete/`` if using the included URLConf. To
     change this, point that named pattern at another URL, or pass your
     preferred URL as the keyword argument ``success_url``.
-    
+
     By default, ``registration.forms.RegistrationForm`` will be used
     as the registration form; to change this, pass a different form
     class as the ``form_class`` keyword argument. The form class you
     specify must have a method ``save`` which will create and return
     the new ``User``.
-    
+
     By default, use the template
     ``registration/registration_form.html``; to change this, pass the
     name of a template as the keyword argument ``template_name``.
-    
+
     **Required arguments**
-    
+
     None.
-    
+
     **Optional arguments**
-    
+
     ``form_class``
         The form class to use for registration.
-    
+
     ``extra_context``
         A dictionary of variables to add to the template context. Any
         callable object in this dictionary will be called to produce
         the end result which appears in the context.
-    
+
     ``success_url``
         The URL to redirect to on successful registration.
-    
+
     ``template_name``
         A custom template to use.
-    
+
     **Context:**
-    
+
     ``form``
         The registration form.
-    
+
     Any extra variables supplied in the ``extra_context`` argument
     (see above).
-    
+
     **Template:**
-    
+
     registration/registration_form.html or ``template_name`` keyword
     argument.
-    
-    """    
+
+    """
     if request.method == 'POST':
         form = form_class(data=request.POST, files=request.FILES, request=request)
         domain_override = request.get_host()
         if form.is_valid():
             new_user = form.save(domain_override)
-            
+
             # Send welcome email
             try:
-                mail.send_mail(sender="olp@mopub.com",
+                mail.send_mail(sender="MoPub, Inc. <olp@mopub.com>",
                            reply_to="support@mopub.com",
                            to=new_user.email,
                            subject="Welcome to MoPub!",
@@ -158,7 +158,7 @@ Thanks!
 The MoPub Team"""%(new_user.first_name))
             except:
                 pass
-            
+
             # success_url needs to be dynamically generated here; setting a
             # a default value using reverse() will cause circular-import
             # problems with the default URLConf for this application, which
@@ -170,7 +170,7 @@ The MoPub Team"""%(new_user.first_name))
             return HttpResponseRedirect(success_url or reverse('publisher_app_create')+'?reg_complete=1')
     else:
         form = form_class(request=request)
-    
+
     if extra_context is None:
         extra_context = {}
     context = RequestContext(request)
@@ -184,21 +184,21 @@ The MoPub Team"""%(new_user.first_name))
 def settings_change(request,
                       success_url=None,
                       form_class=ChangeSettingsForm,
-                      template_name='registration/settings_change_form.html',):                    
-  return register(request, 
+                      template_name='registration/settings_change_form.html',):
+  return register(request,
                   success_url=reverse('account_index'),
                   form_class=form_class,
                   template_name=template_name,
                   extra_context=None,
                   auto_login=False)
-                                                  
-@google_login_required                    
+
+@google_login_required
 def register_google(request,
                         success_url=None,
                         form_class=MPGoogleRegistrationForm,
                         template_name='registration/registration_google_form.html',
                         extra_context=None):
-    return register(request, 
+    return register(request,
                     success_url=success_url,
                     form_class=form_class,
                     template_name=template_name,

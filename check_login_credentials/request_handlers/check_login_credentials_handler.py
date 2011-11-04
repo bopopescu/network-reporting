04693f09f7d0f749292e9aa47bb61cc4f1adb700
Tiago Bandeira <tiago@mopub.com>
2011-11-04 00:42:49 +0000

    EC2 for username encryption working

--- check_login_credentials/request_handlers/check_login_credentials_handler.py
+++ check_login_credentials/request_handlers/check_login_credentials_handler.py
@@ -61,10 +61,10 @@ class CheckLoginCredentialsHandler(tornado.web.RequestHandler):
             login_credentials = AdNetworkLoginCredentials()
             login_credentials.ad_network_name = form.cleaned_data[
                     'ad_network_name']
-            login_credentials.username = form.cleaned_data['username']
+            login_credentials.username = form.cleaned_data['username_str']
             logging.warning('cleaned data')
             logging.warning(form.cleaned_data)
-            login_credentials.password = form.cleaned_data['password2']
+            login_credentials.password = form.cleaned_data['password_str']
             login_credentials.client_key = form.cleaned_data['client_key']
             logging.warning(login_credentials.__dict__)
 

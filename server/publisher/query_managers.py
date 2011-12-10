b262567389422cef2de613a77da07beaa315626c
Nafis Jamal <nafisjamal@mopub.com>
2011-12-10 14:11:05 -0800

    added a comment

--- server/publisher/query_managers.py
+++ server/publisher/query_managers.py
@@ -19,7 +19,7 @@ from google.appengine.api import memcache
 from ad_server.debug_console import trace_logging
 from ad_server.adunit_context.adunit_context import AdUnitContext, CreativeCTR
 from common.constants import MAX_OBJECTS
-CACHE_TIME = 0 # turned off caching
+CACHE_TIME = 0 # turned off cache expiration
 
 class AdUnitContextQueryManager(CachedQueryManager):
     """ Keeps an up-to-date version of the AdUnit Context in memcache.

65d82e536e86a8b9ed0fb245220eec2618925c66
Tiago Bandeira <tiago@mopub.com>
2011-12-12 12:06:42 -0800

    Iad fix for when credentials are wrong, jumptap fix for multiple app pub ids that are the same.

--- server/ad_network_reports/query_managers.py
+++ server/ad_network_reports/query_managers.py
@@ -436,7 +436,8 @@ class AdNetworkReportQueryManager(CachedQueryManager):
                     include_apps=False).next()
 
         for app in AppQueryManager.get_apps_with_network_configs(self.account):
-            pub_id = getattr(app.network_config, '%s_pub_id' % ad_network_name, None)
+            pub_id = getattr(app.network_config, '%s_pub_id' % ad_network_name,
+                    None)
             if pub_id:
                 yield pub_id
 

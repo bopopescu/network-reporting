3088fa85d16816646d3197ad8c4ac90a13e9492b
John Pena <john@mopub.com>
2011-12-12 16:06:35 -0800

    fixing datatables

--- server/public/js/reports-ajaxPoll.js
+++ server/public/js/reports-ajaxPoll.js
@@ -31,7 +31,39 @@ function buildTable(table) {
         'bJQueryUI':true,
         'aLengthMenu': [[50,100,-1], [50, 100, 'All']],
         'iDisplayLength' : 50,
-        'aoColumnDefs': [ {'asSorting': ['desc', 'asc'], 'aTargets': [0,1,2,3,4]},]
+        'aoColumnDefs': [
+            {
+                'asSorting': ['desc', 'asc'],
+                'aTargets': [0,1,2,3,4]
+            }
+        ],
+        "fnDrawCallback": function ( settings ) {
+            if ( settings.aiDisplay.length == 0 )
+            {
+                return;
+            }
+
+            var table_rows = $('tbody tr', settings.nTable);
+            var number_of_columns = table_rows[0].getElementsByTagName('td').length;
+            var sLastGroup = "";
+            for (var i=0; i < table_rows.length; i++)
+            {
+                var display_index = settings._iDisplayStart + i;
+                var sGroup = settings.aoData[settings.aiDisplay[display_index]]._aData[0];
+                if ( sGroup != sLastGroup )
+                {
+                    var nGroup = document.createElement( 'tr' );
+                    var nCell = document.createElement( 'td' );
+                    nCell.colSpan = number_of_columns;
+                    nCell.className = "group";
+                    nCell.innerHTML = sGroup;
+                    nGroup.appendChild( nCell );
+                    table_rows[i].parentNode.insertBefore( nGroup, table_rows[i] );
+                    sLastGroup = sGroup;
+                }
+            }
+        },
+        "aaSorting": [[ 1, 'asc' ]]
     });
 }
 

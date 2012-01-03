fd46d1a2afcd5241161ac1bb1f0458255e4ff48a
Nick Grabenstein <nick@mopub.com>
2012-01-03 12:03:35 -0800

    MORE QUEUES

--- server/stats/stats_accumulator.py
+++ server/stats/stats_accumulator.py
@@ -4,7 +4,7 @@ import random
 import urllib
 
 from ad_server.debug_console import trace_logging
-from common.utils import helpers 
+from common.utils import helpers
 from google.appengine.api import memcache
 from google.appengine.api import taskqueue
 from google.appengine.api import urlfetch
@@ -36,7 +36,7 @@ SHARD_INTERVAL = 10
 MEMCACHE_ALIVE_TIME = 0#6*TIME_BUCKET
 
 REQ_QUEUE_NAME = "network-request-%02d"
-NUM_REQ_QUEUES = 40
+NUM_REQ_QUEUES = 50
 
 
 INAPP_QUEUE_NAME = 'inapp-recorder-%02d'
@@ -57,79 +57,79 @@ def log(request,
         testing=False,
         country_code=None,
         request_id=None):
-    # if this is the second request because of a 
-    # native failure we just bail in order to 
+    # if this is the second request because of a
+    # native failure we just bail in order to
     # Note if logging an adnetwork request, we pass
     # in request = None.
     if request:
         exclude_creatives = request.get_all("exclude")
         if exclude_creatives:
-            return    
+            return
 
     # get parameters from the request or args
-    adunit_id = adunit_id #wat 
+    adunit_id = adunit_id #wat
     if adunit:
         adunit_id = adunit_id or str(adunit.key())
-    creative_id = creative_id 
+    creative_id = creative_id
     if creative:
-        creative_id = creative_id or str(creative.key()) 
-    
+        creative_id = creative_id or str(creative.key())
+
     if request:
         adunit_id = adunit_id or request.get('id', None)
         creative_id = creative_id or request.get('cid', None)
         udid = udid or request.get('udid', None)
         request_id = request.get('reqcnt',None)
         instance_id = request.get('inst',None)
-        country_code = helpers.get_country_code(headers=request.headers)            
+        country_code = helpers.get_country_code(headers=request.headers)
         revenue = request.get('rev',None)
     else:
         # This will always be the case when coming from ad_auction
-        instance_id = None 
+        instance_id = None
         revenue = None
-    
+
     # if trying to record the request of a adunit and creative
     # i.e. request of a network creative
     # we add a "fire-and-forget" taskqueue entry so that the
-    # data shows up in the apache-style request logs    
+    # data shows up in the apache-style request logs
     if adunit_id and creative_id and event == REQ_EVENT:
         trace_logging.info("fire and forget--adunit: %s creative:%s"%(adunit_id,creative_id))
         fire_forget_params = dict(id=adunit_id, cid=creative_id, udid=udid or '', ua=user_agent)
         task = taskqueue.Task(params=fire_forget_params,
                               method='GET',
                               url='/m/req')
-        queue_num = random.randint(0,NUM_REQ_QUEUES-1)                      
+        queue_num = random.randint(0,NUM_REQ_QUEUES-1)
         queue_name = REQ_QUEUE_NAME%queue_num
         try:
             task.add(queue_name)
         except Exception, e:
             trace_logging.warning(e)
-        
+
         # make async internal request to appengine
         # rpc = urlfetch.create_rpc(deadline=2) # in seconds
-        # urlfetch.make_fetch_call(rpc=rpc, 
+        # urlfetch.make_fetch_call(rpc=rpc,
         #                          url="http://38-attempt.mopub-inc.appspot.com/m/req?"+urllib.urlencode(fire_forget_params),
         #                          headers={'X-URLFetch-Service-Id': 'APPSPOT'},
-        #                          follow_redirects=False,)                
-        
+        #                          follow_redirects=False,)
+
     # get account name from the adunit
     if not adunit:
         adunit_context = AdUnitContextQueryManager.cache_get_or_insert(adunit_id)
         adunit = adunit_context.adunit
-    
+
     account = adunit.account
     account_name = str(account.key())
-        
+
     account_shard = _get_shard_num(account,adunit_id,creative_id)
-    
+
     # calculate time bucket
     now = time.time()
     time_bucket = int(now)/TIME_BUCKET # maybe divide by 10 for every second
-    
-    
+
+
     # get the new index for this account and time bucket
     index_key = INDEX_KEY_FORMAT%dict(account_name=account_name,account_shard=account_shard,time=time_bucket)
     log_index = memcache.incr(index_key,initial_value=0) # starts at 1
-    
+
     logging_data = dict(event=event,
                         adunit=adunit_id.replace(r"\'",""), # cleanup for mobile web
                         creative=creative_id,
@@ -139,30 +139,30 @@ def log(request,
                         udid=udid,
                         req=request_id,
                         inst=instance_id)
-                        
-    
+
+
     # bail early if the memcache increment failed
-    if log_index is None or log_index == '': 
+    if log_index is None or log_index == '':
         trace_logging.error("NOOOOO INDEX")
         return
-    
+
     # put the log data into appropriate place
     trace_logging.info("adding to cache: %s ts: %s"%(log_index,time_bucket))
     log_key = LOG_KEY_FORMAT%dict(account_name=account_name,account_shard=account_shard,time=time_bucket,log_index=log_index)
     memcache.set(log_key,logging_data,time=MEMCACHE_ALIVE_TIME)
     trace_logging.info("done adding to cache")
-    
-    
+
+
     # send to appropriately named task_queue
-    task_name = TASK_NAME%dict(account_name=account_name.replace('_','1X--X1'), # must escape '_' regex: [a-zA-Z0-9-]{1,500}$   
+    task_name = TASK_NAME%dict(account_name=account_name.replace('_','1X--X1'), # must escape '_' regex: [a-zA-Z0-9-]{1,500}$
                                account_shard=account_shard,
                                time=time_bucket)
 
     # because we have limited task queue API calls we only try adding
-    # to the queue up to MAX_TASK_ADDS time, most are dupes anyway                           
+    # to the queue up to MAX_TASK_ADDS time, most are dupes anyway
     if log_index > MAX_TASK_ADDS:
         return
-        
+
     try:
         trace_logging.info("trying to add to queue (in try): %s"%task_name)
         account_bucket = hash(account_name)%NUM_TASK_QUEUES
@@ -179,23 +179,23 @@ def log(request,
             t.add(task_queue_name)
     except taskqueue.TaskAlreadyExistsError:
         trace_logging.info("task %s already exists"%task_name)
-    except Exception, e:    
+    except Exception, e:
         trace_logging.error(e)
-        
+
 def _get_time_bucket_start(time_bucket):
     # time_bucket = the current time bucket
     # TIME_BUCKET = the time size of a bucket
-    return time_bucket*TIME_BUCKET       
-    
+    return time_bucket*TIME_BUCKET
+
 def _get_execution_datetime(account_shard,time_bucket):
     time_bucket_start = _get_time_bucket_start(time_bucket)
     execution_time = time_bucket_start + TIME_BUCKET + TIME_BUFFER + SHARD_INTERVAL*account_shard
     return datetime.datetime.fromtimestamp(execution_time)
-        
+
 
 def _get_shard_num(account,adunit_id,creative_id):
     """
-        Returns the 0 indexed shard number for the account based on 
+        Returns the 0 indexed shard number for the account based on
         the adunit and creative combo
     """
     account_num_shards = account.number_shards or 1
@@ -211,11 +211,11 @@ def log_inapp_purchase(request, event, udid, receipt, mobile_appid=None):
     task = taskqueue.Task(params=dict(udid=udid, receipt=receipt, mobile_appid=mobile_appid, time=time.time()),
                           method='POST',
                           url='/m/purchase_txn')
-    queue_num = random.randint(0,NUM_INAPP_QUEUS-1)                      
+    queue_num = random.randint(0,NUM_INAPP_QUEUS-1)
     queue_name = INAPP_QUEUE_NAME%queue_num
-    
+
     try:
         task.add(queue_name)
     except Exception, e:
         trace_logging.error(e)
-    
+

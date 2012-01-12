a1192757059ecf3e922650028eb5488cd8edbb19
Nafis Jamal <nafisjamal@mopub.com>
2012-01-11 23:11:37 -0800

    added deployment scripts

---
+++ server/adserver_deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "copying 'app.adserver.yaml' -> 'app.yaml'"
+cp app.adserver.yaml app.yaml
+echo "uploading to GAE"
+appcfg.py update .
+echo "copying 'app.frontend.yaml' -> 'app.yaml'"
+# cp app.frontend.yaml app.yaml

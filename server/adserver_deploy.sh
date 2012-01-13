b5e62371a4ea5fe1d7f40d1d4b92e16e4101766c
Nafis Jamal <nafisjamal@mopub.com>
2012-01-13 12:01:10 -0800

    fixed python version

--- server/adserver_deploy.sh
+++ server/adserver_deploy.sh
@@ -2,6 +2,6 @@
 echo "copying 'app.adserver.yaml' -> 'app.yaml'"
 cp app.adserver.yaml app.yaml
 echo "uploading to GAE"
-appcfg.py update .
+python2.5 appcfg.py update .
 echo "copying 'app.frontend.yaml' -> 'app.yaml'"
 # cp app.frontend.yaml app.yaml

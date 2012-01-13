df489aa3b142263da530c7aaba5d618c3db090a8
Nafis Jamal <nafisjamal@mopub.com>
2012-01-13 12:55:46 -0800

    added comment

--- server/adserver_deploy.sh
+++ server/adserver_deploy.sh
@@ -5,15 +5,21 @@
 # app.yaml file.
 # Nafis 1/13/2012
 
-echo "copying ./server -> /tmp/server"
 pushd ..
+
+echo "copying ./server -> /tmp/server"
 cp -r server /tmp/server
 
+# manipulating the copied folder
 cd /tmp/server
+
 echo "copying 'app.adserver.yaml' -> 'app.yaml'"
 cp app.adserver.yaml app.yaml
+
 echo "uploading to GAE"
 python2.5 appcfg.py update .
+
 echo "copying 'app.frontend.yaml' -> 'app.yaml'"
 cp app.frontend.yaml app.yaml
+
 popd

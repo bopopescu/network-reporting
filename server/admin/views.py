d610b5c0a884fa7b7ee408c2d7f6736b1c343919
Nafis Jamal <nafisjamal@mopub.com>
2012-01-31 17:11:05 -0800

    removed logging and fixed start_date bug

--- server/admin/views.py
+++ server/admin/views.py
@@ -85,9 +85,9 @@ def dashboard_prep(request, *args, **kwargs):
     
     
     days = StatsModel.lastdays(NUM_DAYS)
-    logging.info('days: %s', days)
     # gets all undeleted applications
-    start_date = datetime.date.today() - datetime.timedelta(days=NUM_DAYS) # NOTE: change
+    start_date = StatsModel.today() - datetime.timedelta(days=NUM_DAYS) # NOTE: change
+    logging.warning('start_date: %s days :%s', start_date, days)
 
     apps = AppQueryManager.get_all_apps()
     

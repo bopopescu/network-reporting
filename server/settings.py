583fa427450e6181ec955fada6f8143e6b681485
Rob McQueen <robmcqn@gmail.com>
2012-02-03 14:09:06 -0800

    merging fixtures-patch with master

--- server/settings.py
+++ server/settings.py
@@ -17,7 +17,7 @@
 from common.ragendja.settings_pre import *
 
 NEW_UI = True
-
+DEBUG = False
 DEFAULT_FROM_EMAIL = 'olp@mopub.com'
 SERVER_EMAIL = 'olp@mopub.com'
 
@@ -160,3 +160,9 @@ LOGIN_REQUIRED_PREFIXES = (
 ACCOUNT_ACTIVATION_DAYS = 14
 
 from common.ragendja.settings_post import *
+
+#add additional settings for local machine settings
+try:
+    from local_settings import *
+except ImportError:
+    pass

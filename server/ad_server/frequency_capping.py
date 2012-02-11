220045315214bd71b007bcb45c7a0cfb4f944618
Nafis Jamal <nafisjamal@mopub.com>
2012-02-11 13:49:48 -0600

    ensure keys are not ambiguous

--- server/ad_server/frequency_capping.py
+++ server/ad_server/frequency_capping.py
@@ -5,10 +5,10 @@ from google.appengine.ext import db
 
 ############## HELPER FUNCTIONS ################
 def memcache_key_for_date(udid, datetime, db_key):
-    return '%s:%s:%s' % (udid, datetime.strftime('%d'), db_key)
+    return '%s:d%s:%s' % (udid, datetime.strftime('%d'), id_or_key_name(db_key))
 
 def memcache_key_for_hour(udid, datetime, db_key):
-    return '%s:%s:%s' % (udid, datetime.strftime('%H'), db_key)
+    return '%s:h%s:%s' % (udid, datetime.strftime('%H'), id_or_key_name(db_key))
 
 def id_or_key_name(db_key):
     """

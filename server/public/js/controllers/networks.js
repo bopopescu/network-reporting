41065771baa9bc0f187e16f252ee5cfc2e556195
Tiago Bandeira <tiago@mopub.com>
2012-03-06 18:30:09 -0800

    Removed dependencies on the ad_network_report's controller for the network details page

--- server/public/js/controllers/networks.js
+++ server/public/js/controllers/networks.js
@@ -1,13 +1,4 @@
 $(function() {
-    // TODO: document
-    /*
-     *   adgroups_data
-     *   graph_start_date
-     *   today
-     *   yesterday
-     *   ajax_query_string
-     */
-
     var toast_error = function () {
          var message = $("Please <a href='#'>refresh the page</a> and try again.")
             .click(function(e){
@@ -70,6 +61,44 @@ $(function() {
         }
     }
 
+    var NetworkDetailsController = { 
+        initialize: function(bootstrapping_data) {
+            var network_data = bootstrapping_data.network_data,
+                graph_start_date = bootstrapping_data.graph_statr_date,
+                today = bootstrapping_data.today,
+                yesterday = bootstrapping_data.yesterday,
+                ajax_query_string = bootstrapping_data.ajax_query_string;
+
+            // TODO: make functions and call them
+            // Load rolled up network stats
+            $.each(network_data, function(index, network) {
+                
+                var roll_up = new RollUp({
+                    id: network.name,
+                    type: 'network'
+                });
+                var roll_up_view = new RollUpView({
+                    model: roll_up
+                });
+                roll_up.fetch({ data: ajax_query_string });
+            });
+            
+            // Load stats for app on network
+            $.each(network_data, function(index, network) {
+                if(network.models.length > 0) {
+                    var apps_on_network = new AppOnNetworkCollection(network.models);
+                    apps_on_network.each(function(app_on_network) {
+                        var app_on_network_view = new AppOnNetworkView({
+                            model: app_on_network
+                        });
+                        app_on_network.fetch({ data: ajax_query_string });
+                    });
+                }
+            });
+        }
+    }
+
+    window.NetworkDetailsController = NetworkDetailsController;
     window.NetworksController = NetworksController;
 });
 

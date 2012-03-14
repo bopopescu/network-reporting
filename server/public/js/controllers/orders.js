391317e4d3a969c71502a816dfb6fe4335b61585
John Pena <john@mopub.com>
2012-03-13 18:08:53 -0700

    orders.js

---
+++ server/public/js/controllers/orders.js
@@ -0,0 +1,29 @@
+(function () {
+    "use strict";
+
+    var OrdersController = {
+        initializeIndex: function(bootstrapping_data) {
+            
+        },
+
+        initializeOrderDetail: function(bootstrapping_data) {
+            
+        },
+
+        initializeLineItemDetail: function(bootstrapping_data) {
+            
+        },
+
+        initializeOrderForm: function(bootstrapping_data) {
+            
+        },
+
+
+        initializeLineItemForm: function(bootstrapping_data) {
+                    
+        }
+    };
+
+    window.OrdersController = OrdersController;
+
+})(window.jQuery);

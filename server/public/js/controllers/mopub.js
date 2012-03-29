4479c49b2c9cd1ae0bb06e50ffab81dbc35056a9
John Pena <john@mopub.com>
2012-03-29 14:33:11 -0700

    moving around dateOptions

--- server/public/js/controllers/mopub.js
+++ server/public/js/controllers/mopub.js
@@ -24,8 +24,97 @@ if (typeof window.console == "undefined") {
     var Stats = mopub.Stats || {};
     var Utils = mopub.Utils || {};
 
+        /*
+     * ## initializeDateButtons
+     * Loads all click handlers/visual stuff for the date buttons. Used
+     * on a ton of pages, probably could be refactored by someone brave
+     * enough.
+     */
+    function initializeDateButtons () {
+        $('#dashboard-dateOptions input').click(function() {
+            var option = $(this).val();
+            if (option == 'custom') {
+                $('#dashboard-dateOptions-custom-modal').dialog({
+                    width: 570,
+                    buttons: [
+                        {
+                            text: 'Set dates',
+                            css: { fontWeight: '600' },
+                            click: function() {
+                                var from_date = $('#dashboard-dateOptions-custom-from').datepicker("getDate");
+                                var to_date = $('#dashboard-dateOptions-custom-to').datepicker("getDate");
+                                var num_days = Math.ceil((to_date.getTime()-from_date.getTime())/(86400000)) + 1;
+
+                                var from_day = from_date.getDate();
+                                // FYI, months are indexed from 0
+                                var from_month = from_date.getMonth() + 1;
+                                var from_year = from_date.getFullYear();
+
+                                $(this).dialog("close");
+                                var location = document.location.href.replace(/\?.*/,'');
+                                document.location.href = location
+                                    + '?r=' + num_days
+                                    + '&s=' + from_year + "-" + from_month + "-" + from_day;
+                            }
+                        },
+                        {
+                            text: 'Cancel',
+                            click: function() {
+                                $(this).dialog("close");
+                            }
+                        }
+                    ]
+                });
+            } else {
+                // Tell server about selected option to get new data
+                var location = document.location.href.replace(/\?.*/,'');
+                document.location.href = location + '?r=' + option;
+            }
+        });
+
+
+        // set up stats breakdown dateOptions
+        $('#stats-breakdown-dateOptions input').click(function() {
+            $('.stats-breakdown-value').hide();
+            $('.stats-breakdown-value.'+$(this).val()).show();
+        });
+
+        // set up custom dateOptions modal dialog
+        $('#dashboard-dateOptions-custom-from').datepicker({
+            defaultDate: '-15d',
+            maxDate: '0d',
+            onSelect: function(selectedDate) {
+                var other = $('#dashboard-dateOptions-custom-to');
+                var instance = $(this).data("datepicker");
+                var date = $.datepicker.parseDate(instance.settings.dateFormat
+                                                  || $.datepicker._defaults.dateFormat,
+                                                  selectedDate,
+                                                  instance.settings);
+                other.datepicker('option', 'minDate', date);
+            }
+        });
+
+        $('#dashboard-dateOptions-custom-to').datepicker({
+            defaultDate: '-1d',
+            maxDate: '0d',
+            onSelect: function(selectedDate) {
+                var other = $('#dashboard-dateOptions-custom-from');
+                var instance = $(this).data("datepicker");
+                var date = $.datepicker.parseDate(instance.settings.dateFormat ||
+                                                  $.datepicker._defaults.dateFormat,
+                                                  selectedDate,
+                                                  instance.settings);
+                other.datepicker('option', 'maxDate', date);
+            }
+        });
+    }
+
+    
+
     $(document).ready(function() {
 
+        initializeDateButtons();
+        
         /*
          * ## Mixpanel Event Tracking
          */

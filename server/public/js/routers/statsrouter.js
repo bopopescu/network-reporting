35f3425ba4873e6369baade6fe786e24e80d568c
John Pena <john@mopub.com>
2012-04-02 00:58:09 -0700

    stubbed out the router for the date/granularity picker

---
+++ server/public/js/routers/statsrouter.js
@@ -0,0 +1,23 @@
+(function($, Backbone, _) {
+
+    var StatsRouter = Backbone.Router.extend({
+        routes : {
+            "/daily/start/:start_date/end/:end_date": "fetchStatsForDays",
+            "/hourly/start/:start_date/end/:end_date": "fetchStatsForHours",
+            "/weekly/start/:start_date/end/:end_date": "fetchStatsForWeeks",
+        },
+        fetchStatsForDays: function(start_date, end_date) {
+            var granularity = 'daily';
+        },
+        fetchStatsForHours: function(start_date, end_date) {
+            var granularity = 'hourly';
+
+        },
+        fetchStatsForWeeks: function(start_date, end_date) {
+            var granularity = 'weekly';
+        }
+
+    });
+
+
+})(window.jQuery, window.Backbone, window._);

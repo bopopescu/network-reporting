3000da78b70065e835a2dd64fc5b8657e921bfde
James Sun <james@mopub.com>
2012-04-05 17:14:22 -0700

    AWS components added

---
+++ bd/autojohnchen/hive_job_launcher.py
@@ -0,0 +1,162 @@
+import logging
+import os
+import sys
+import time
+import traceback
+from datetime import datetime
+
+import boto
+from boto.emr.connection import EmrConnection
+from boto.emr.instance_group import InstanceGroup
+from boto.emr.step import JarStep
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+
+
+# create the EMR connection
+ACCESS_KEY_ID = 'AKIAJKOJXDCZA3VYXP3Q'
+SECRET_ACCESS_KEY = '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'
+EMR_CONN = EmrConnection(ACCESS_KEY_ID, SECRET_ACCESS_KEY)
+S3_CONN = S3Connection(ACCESS_KEY_ID, SECRET_ACCESS_KEY)
+
+S3_BUCKET = S3_CONN.get_bucket('mopub-aws-logging')
+S3_ROOT = 's3://mopub-aws-logging'
+S3_LOG_URI = S3_ROOT + '/jobflow_logs'
+
+AZ = 'us-east-1'
+AWS_HIVE_LIB_DIR = 's3://%s.elasticmapreduce/libs/hive/' % (AZ)
+SCRIPT_RUNNER_JAR = 's3://%s.elasticmapreduce/libs/script-runner/script-runner.jar' % (AZ)
+AWS_HIVE_VER = '0.7.1'
+
+JOBFLOW_NAME = 'jc automator hive job'
+SCHEMA_VERSION = 2
+USER='autojohnchen'
+QUERY_FILE = 'monthly_mpx_rev_by_app.q'
+
+
+def get_jobflow_state(jobid):
+    while True:
+        try:
+            state = EMR_CONN.describe_jobflow(jobid).state
+            return state
+        except:
+            time.sleep(1)
+            logging.info('try to get waiting jobflow state again...')
+
+
+def get_output_data(s3_path):
+    content = ''
+    rs = S3_BUCKET.list(s3_path)
+    for f in rs:
+        content += f.get_contents_as_string()
+    return content
+
+
+def get_waiting_jobflow():
+    waiting_jobflows = EMR_CONN.describe_jobflows(['WAITING'])
+    for jobflow in waiting_jobflows:
+        if jobflow.name != JOBFLOW_NAME: continue
+
+        jobid = jobflow.jobflowid
+        num_steps = len(jobflow.steps)
+        logging.info('found waiting jobflow %s "%s" with %i steps completed' % (jobid, JOBFLOW_NAME, num_steps))
+
+        if num_steps > 250:
+            logging.info('num of steps near limit of 256: terminating jobflow %s "%s" ...' % (jobid, JOBFLOW_NAME))
+            EMR_CONN.terminate_jobflow(jobid)
+        else:
+            return jobid
+    return None
+
+
+def launch_monthly_mpx_rev_hivejob(year, month, num_machines=60):
+    # create master and core instance groups
+    instance_groups = [
+        InstanceGroup(num_instances=1, role='MASTER', type='m1.large', market='ON_DEMAND', name='master-group'),
+    ]
+
+    # populate hive args
+    hive_args = []
+    hive_args += ['v=%i' % SCHEMA_VERSION]
+    hive_args += ['y=%i' % year]  # current year
+    hive_args += ['m=%02i' % month]  # current month
+
+    # add username and filename as hive args, accessible inside hive query file
+    hive_args += ['user=%s' %USER, 'qf=%s' %QUERY_FILE]
+
+    logging.info('hive args: %s', hive_args)
+
+    # prepend S3 URI path to hive script file
+    # s3://mopub-aws-logging/hive_scripts/autojohnchen/monthly_mpx_rev_by_app.q
+    s3_query_file = '%s/hive_scripts/%s/%s' % (S3_ROOT, USER, QUERY_FILE)
+
+
+    # jar step to setup Hive on EMR machine
+    setup_step = JarStep(
+        name='setup hive',
+        jar=SCRIPT_RUNNER_JAR,
+        step_args=[AWS_HIVE_LIB_DIR + 'hive-script',
+            '--base-path', AWS_HIVE_LIB_DIR,
+            '--hive-versions', AWS_HIVE_VER,
+            '--install-hive'],
+    )
+
+    # basic step args for running hive script
+    script_step_args = [AWS_HIVE_LIB_DIR + 'hive-script',
+        '--base-path', AWS_HIVE_LIB_DIR,
+        '--hive-versions', AWS_HIVE_VER,
+        '--run-hive-script',
+        '--args',
+        '-f', s3_query_file]
+
+    # add additional hive args from CLI
+    for pair in hive_args:
+        script_step_args.extend(['-d', pair])
+
+    # jar step running the Hive script
+    script_step = JarStep(
+        name=QUERY_FILE,
+        jar=SCRIPT_RUNNER_JAR,
+        step_args=script_step_args,
+    )
+
+
+    # try to find an existing jobflow in waiting mode
+    while True:
+        try:
+            jobid = get_waiting_jobflow()
+            break
+        except:
+            time.sleep(1)
+            logging.info('try to get waiting jobflows again...')
+
+    if jobid:
+        steps = [script_step]   # no need for setup step
+        EMR_CONN.add_jobflow_steps(jobid, steps)
+        logging.info('added step to waiting jobflow: %s "%s"' %(jobid, JOBFLOW_NAME))
+    else:   # spin up a new jobflow
+        steps = [setup_step, script_step]
+        jobid = EMR_CONN.run_jobflow(
+            name=JOBFLOW_NAME,
+            ec2_keyname='jpmopub',
+            steps=steps,
+            log_uri=S3_LOG_URI,
+            instance_groups=instance_groups,
+            # keep_alive=True,
+            enable_debugging=True,
+            hadoop_version='0.20.205',
+            ami_version='2.0.4',
+        )
+        logging.info('submitted new jobflow: %s "%s"' %(jobid, JOBFLOW_NAME))
+
+    # output dir containing hive results:
+    # s3://mopub-aws-logging/hive_outputs/autojohnchen/monthly_mpx_rev_by_app.q/v=2/y=${y}/m=${m}/
+    output_dir = 'hive_outputs/%s/%s/v=%s/y=%s/m=%s' % (USER, QUERY_FILE, SCHEMA_VERSION, year, month)
+
+    return jobid, output_dir
+
+
+
+if __name__ == '__main__':
+    # for testing out
+    launch_monthly_mpx_rev_hivejob(2012, 4, 40)
--- bd/autojohnchen/main.py
+++ bd/autojohnchen/main.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 #
 #
+import logging, email
+import datetime, time, calendar
+
 from google.appengine.ext import webapp
 from google.appengine.ext import deferred
 from google.appengine.ext.webapp import util
-from google.appengine.ext.webapp.mail_handlers import InboundMailHandler 
+from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
 from google.appengine.api import mail, urlfetch, taskqueue
-import logging, email
-import datetime, time, calendar 
+
+from hive_job_launcher import launch_monthly_mpx_rev_hivejob, get_jobflow_state, get_output_data
+
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
@@ -28,7 +32,7 @@ class MainHandler(webapp.RequestHandler):
                                          <option value='9'>Sep</option>
                                          <option value='10'>Oct</option>
                                          <option value='11'>Nov</option>
-                                         <option value='12'>Dec</option></select>                                         
+                                         <option value='12'>Dec</option></select>
                         <select name='y'><option>2011</option><option selected>2012</option><option>2013</option></select>
                         <input type='submit' value='Generate'></input>
                     </form>
@@ -37,21 +41,16 @@ class MainHandler(webapp.RequestHandler):
 
 
 class GenerateHandler(webapp.RequestHandler):
-    def _initiate_map_reduce(self, m, y):
-        # TODO: XS please complete this... 
-        q = 'http://www.google.com/q?start=%d%02d%02d&end=%d%02d%02d' % (y, m, 1, y, m, calendar.monthrange(y, m)[1])
-        return "ABC123"
-        
     def post(self):
         # fire off reporting API request
         m = int(self.request.get('m'))
         y = int(self.request.get('y'))
-        jobId = self._initiate_map_reduce(m, y)
-        
+        jobId, s3_dir = launch_monthly_mpx_rev_hivejob(y, m)
+
         # initiates a task queue item that checks to see if this job has completed
-        taskqueue.add(url="/check", params={"id": jobId}, queue_name='jobcheck', countdown=60)
-        
-        # play robot movie for user 
+        taskqueue.add(url="/check", params={"id": jobId, "s3_dir": s3_dir}, queue_name='jobcheck', countdown=60)
+
+        # play robot movie for user
         self.response.out.write(
             """<html><head>
                     <script type='text/javascript' src='/images/jwplayer.js'></script>
@@ -74,60 +73,61 @@ class GenerateHandler(webapp.RequestHandler):
                     </script>
                 </body>
                </html>""" % jobId)
-               
+
 class CheckHandler(webapp.RequestHandler):
     # Returns True if the job has completed, False if still running
     def _is_job_complete(self, jobId):
-        # TODO -- XS please complete this
-        return False
-        
-    # Returns S3 directory if the job has completed successfully 
-    # or None if the job failed 
-    def _get_job_output(self, jobId):
-        # TODO -- XS please complete this 
-        return "http://www.google.com"
-        
+        state = get_jobflow_state(jobId)
+        logging.info('%s\tjob %s state: %s' % (time.strftime('%b %d %Y %H:%M:%S'), jobId, state))
+        if state == ['COMPLETED', 'WAITING']:
+          return True, True   # done and successful
+        if state in ['FAILED', 'TERMINATED']:
+          return True, False  # done but failed
+        return False, False   # still running
+
     # Checks the S3 job - notifies user on failure
-    # handles completed jobs by defering into a task q for later 
+    # handles completed jobs by defering into a task q for later
     def post(self):
         jobId = self.request.get('id')
-        if self._is_job_complete(jobId): 
-            s3_dir = self._get_job_output(jobId)
-            if s3_dir:
-                # OK go iterate through this directory
-                self.collect_job_output(jobId, s3_dir)
-                return
-            else:
-                # This failed so we send a note indicating failure
-                mail.send_mail(sender="Automated John Chen <johnchen@mopub.com>",
-                                  to="johnchen@mopub.com",
-                                  subject="FAILED: Your report from %s could not be completed" % message.date,
-                                  body="""Sir- Sorry, I couldn't get it done. Apologies, Automated John Chen""")
+        s3_dir = self.request.get('s3_dir')
+        done, successful = self._is_job_complete(jobId)
+
+        if done and successful:
+            # OK go iterate through this directory
+            self.collect_job_output(jobId, s3_dir)
+            return
+        elif done and not successful:
+            # This failed so we send a note indicating failure
+            mail.send_mail(sender="Automated John Chen <johnchen@mopub.com>",
+                              to="johnchen@mopub.com",
+                              subject="FAILED: Your report from %s could not be completed" % message.date,
+                              body="""Sir- Sorry, I couldn't get it done. Apologies, Automated John Chen""")
         else:
             # Not complete yet so we fail this task and retry
             logging.info("JobID %s is not ready, retrying" % jobId)
             raise Exception("job not ready - retrying")
-            
+
     def collect_job_output(self, jobId, s3_dir):
         logging.info(s3_dir)
 
         # grab the results
-        x = urlfetch.fetch(s3_dir)
+        content = get_output_data(s3_dir)
 
         # create outbound email
         mail.send_mail(sender="Automated John Chen <johnchen@mopub.com>",
                           to="jim@mopub.com",
                           subject="SUCCESS: Your report with ID %s" % jobId,
                           body="""Sir- Good times, as requested. Sincerely, Automated John Chen""",
-                          attachments=[("%s.csv" % jobId, x.content)])
-        
+                          attachments=[("%s.tsv" % jobId, content)])
+
 
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler), 
-                                          ('/generate', GenerateHandler), 
+    application = webapp.WSGIApplication([('/', MainHandler),
+                                          ('/generate', GenerateHandler),
                                           ('/check', CheckHandler)],
                                          debug=True)
     util.run_wsgi_app(application)
+
 
 if __name__ == '__main__':
     main()

619324e5ce3e0c49ae6093358a1a946288c91fa6
Tiago Bandeira <tiago@mopub.com>
2012-04-23 20:34:01 -0700

    Ready to push to master

--- server/admin/randomgen.py
+++ server/admin/randomgen.py
@@ -323,7 +323,7 @@ def generate_creative(account,adgroup):
 
 
 #Example Method to generate data. See top configuration contants for customizing result
-def main_():
+def main():
     account = generate_account(USERNAME,PASSWORD,USERNAME)
 
     # Create marketplace campaign
@@ -388,7 +388,7 @@ def main_():
 
 # NOTE: main method for new network campaign creation
 # TODO: merge main method above
-def main():
+def main_():
     account = generate_account(USERNAME,PASSWORD,USERNAME,
             display_new_networks=True)
 

ef4beb4d65fdaafbd5d56a39a0f413243952c1d5
John Pena <john@mopub.com>
2012-05-04 18:27:40 -0700

    resolving path issues with selenium tests for hudson

--- server/tests/interaction/__main__.py
+++ server/tests/interaction/__main__.py
@@ -5,11 +5,11 @@ PWD = os.path.dirname(__file__)
 sys.path.append(os.path.join(PWD, '..'))
 
 
-from interaction_tests.account import AccountInteractionTestCase
-from interaction_tests.networks import NetworkInteractionTestCase
-from interaction_tests.marketplace import MarketplaceInteractionTestCase
-from interaction_tests.campaigns import DirectSoldInteractionTestCase
-from interaction_tests.inventory import InventoryInteractionTestCase
+from interaction.account import AccountInteractionTestCase
+from interaction.networks import NetworkInteractionTestCase
+from interaction.marketplace import MarketplaceInteractionTestCase
+from interaction.campaigns import DirectSoldInteractionTestCase
+from interaction.inventory import InventoryInteractionTestCase
 
 if __name__ == "__main__":
     unittest.main()

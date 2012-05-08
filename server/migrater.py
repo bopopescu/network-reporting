ddf5aabe0ebeaba2daa9d7ad3d226f3dbf79b59e
Nafis Jamal <nafisjamal@mopub.com>
2012-05-08 11:09:22 -0700

    added mapreduce yaml

--- server/migrater.py
+++ server/migrater.py
@@ -8,6 +8,7 @@ from common.utils import helpers
 from google.appengine.api import images, files
 from mapreduce import operation as op
 # from reporting.models import StatsModel, MPStatsModel
+from account.models import Account
 from publisher.models import App
 def mapper(s):
     # change entity
@@ -94,5 +95,8 @@ def creative_activater(creative):
 def network_configer(obj):
     network_config = getattr(obj, 'network_config', None)
     if network_config:
-        network_config.account = obj._account  # key only
+        if isinstance(obj, Account):
+            network_config.account = obj.key()  # key only
+        else:
+            network_config.account = obj._account  # key only
         yield op.db.Put(network_config)

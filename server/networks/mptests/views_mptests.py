8331c318833f62a9ff642d5b74014fd72ab08538
Tiago Bandeira <tiago@mopub.com>
2012-05-09 20:17:14 -0700

    Created two new tests for the edit network view

--- server/networks/mptests/views_mptests.py
+++ server/networks/mptests/views_mptests.py
@@ -6,6 +6,7 @@ sys.path.append(os.environ['PWD'])
 import common.utils.test.setup
 
 from django.core.urlresolvers import reverse
+from google.appengine.ext import db
 
 from admin.randomgen import generate_app, generate_adunit
 from common.utils.test.views import BaseViewTestCase
@@ -21,14 +22,30 @@ from advertiser.models import NetworkStates
 from advertiser.query_managers import CampaignQueryManager, \
         AdGroupQueryManager
 
+# Imports for tear down
+from account.models import Account, \
+        NetworkConfig
+from publisher.models import App, \
+        AdUnit
+from advertiser.models import Campaign, \
+        AdGroup, \
+        Creative
+from ad_network_reports.models import AdNetworkLoginCredentials, \
+        AdNetworkAppMapper
+
 DEFAULT_BID = 0.05
 
 class NetworkEditViewTestCase(BaseViewTestCase):
     def setUp(self):
+        _clear_db()
         super(NetworkEditViewTestCase, self).setUp()
 
         app1 = generate_app(self.account)
         adunit1 = generate_adunit(app1, self.account)
+
+    def tearDown(self):
+        _clear_db()
+        super(NetworkEditViewTestCase, self).setUp()
 
     def mptest_create_simple_default_admob_network(self):
         """
@@ -45,23 +62,44 @@ class NetworkEditViewTestCase(BaseViewTestCase):
 
         check_response(self, response, network_type, apps)
 
-    def mptest_create_simple_custom_networks(self):
+    def mptest_create_simple_custom_network(self):
         """
-        Create custom network campaigns of custom and custom_native types with
-        one app and adunit.
+        Create custom network campaign.
 
         Author: Tiago Bandeira
         """
-        pass
+        network_type = 'custom'
+        apps = PublisherQueryManager.get_objects_dict_for_account(
+                account=self.account).values()
+        self.assertEqual(len(apps), 1)
 
-    def mptest_create_simple_defined_custom_networks(self):
+        response = setup_and_make_request(self, network_type, apps)
+
+        check_response(self, response, network_type, apps,
+                network_state=NetworkStates.CUSTOM_NETWORK_CAMPAIGN)
+
+    def mptest_create_simple_admob_custom_network(self):
         """
         Create custom network campaigns of all types excluding custom and
         custom_native with one app and adunit.
 
         Author: Tiago Bandeira
         """
-        pass
+        network_type = 'admob'
+        apps = PublisherQueryManager.get_objects_dict_for_account(
+                account=self.account).values()
+        self.assertEqual(len(apps), 1)
+
+        # Create default admob campaign
+        default_campaign = CampaignQueryManager.get_default_network_campaign(
+                self.account, network_type)
+        default_campaign.put()
+
+        response = setup_and_make_request(self, network_type, apps)
+
+        check_response(self, response, network_type, apps,
+                network_state=NetworkStates.CUSTOM_NETWORK_CAMPAIGN,
+                other_campaigns=[default_campaign])
 
     def mptest_create_complex_default_networks(self):
         """
@@ -76,6 +114,11 @@ class NetworkEditViewTestCase(BaseViewTestCase):
 #
 def setup_and_make_request(test_case, network_type, apps, default_bid=
         DEFAULT_BID):
+    """
+    Setup and make the request to create a network campaign
+
+    Author: Tiago Bandeira
+    """
     # Set constants
     campaign_name = NETWORKS[network_type]
     url = reverse('edit_network', kwargs={'network': network_type})
@@ -113,19 +156,24 @@ def setup_and_make_request(test_case, network_type, apps, default_bid=
     return response
 
 def check_response(test_case, response, network_type, apps,
-        default_bid=DEFAULT_BID):
+        default_bid=DEFAULT_BID, network_state=NetworkStates.
+        DEFAULT_NETWORK_CAMPAIGN, other_campaigns=[]):
+    """
+    Validate the response
+
+    Author: Tiago Bandeira
+    """
     test_case.assertEqual(response.status_code, 200)
 
     # New campaign in memcache?
     network_campaigns = CampaignQueryManager.get_network_campaigns(
             test_case.account, is_new=True)
-    test_case.assertEqual(len(network_campaigns), 1)
+    test_case.assertEqual(len(network_campaigns) - len(other_campaigns), 1)
 
     # Is the campaign set up properly?
-    campaign = network_campaigns[0]
+    campaign = list(set(network_campaigns) - set(other_campaigns))[0]
     test_case.assertEqual(campaign.network_type, network_type)
-    test_case.assertEqual(campaign.network_state, NetworkStates.
-            DEFAULT_NETWORK_CAMPAIGN)
+    test_case.assertEqual(campaign.network_state, network_state)
 
     # Was one adgroup per app created and are the created adgroups set up
     # properly?
@@ -135,5 +183,24 @@ def check_response(test_case, response, network_type, apps,
                     adunit.key(), test_case.account.key(), get_from_db=True)
             test_case.assertTrue(adgroup)
             test_case.assertEqual(adgroup.network_type,
-                    NETWORK_ADGROUP_TRANSLATION[network_type])
+                    NETWORK_ADGROUP_TRANSLATION.get(network_type, network_type))
             test_case.assertEqual(adgroup.bid, default_bid)
+
+def _clear_db():
+    # account models
+    db.delete(Account.all())
+    db.delete(NetworkConfig.all())
+
+    # publisher models
+    db.delete(App.all())
+    db.delete(AdUnit.all())
+
+    # advertiser models
+    db.delete(Campaign.all())
+    db.delete(AdGroup.all())
+    db.delete(Creative.all())
+
+    # ad_network_reports models
+    db.delete(AdNetworkLoginCredentials.all())
+    db.delete(AdNetworkAppMapper.all())
+

dbc404ef7d536caf87b485b8f8c98a14f2a81d4c
Peter Linnartz <peter@mopub.com>
2012-05-14 15:17:33 -0700

    added some test utils, updated publisher view tests

--- server/common/utils/test/test_utils.py
+++ server/common/utils/test/test_utils.py
@@ -4,11 +4,12 @@ import sys
 sys.path.append(os.environ['PWD'])
 
 from google.appengine.ext import db
+from nose.tools import eq_
 
-from advertiser.models import *
-from publisher.models import *
+from account.models import Account
+from advertiser.models import Campaign, AdGroup, Creative
+from publisher.models import App, AdUnit
 from reporting.models import StatsModel
-
 
 
 def prepend_list(e, li):
@@ -21,8 +22,8 @@ def add_lists(list_of_lists):
     for li in list_of_lists[1:]:
         result = map(sum, zip(result, li))
     return result
-    
-    
+
+
 def clear_datastore():
     db.delete(Account.all())
     db.delete(AdUnit.all())
@@ -31,15 +32,45 @@ def clear_datastore():
     db.delete(AdGroup.all())
     db.delete(Campaign.all())
     db.delete(StatsModel.all())
-    
+
 
 def debug_key_name(key_name, id_dict):
     return ':'.join([id_dict.get(id, id) for id in key_name.split(':')])
-    
-    
+
+
 def debug_helper(readable_key_name, expected_counts, actual_counts):
     if expected_counts != actual_counts:
         print
         print readable_key_name
         print 'expected:', expected_counts
         print 'actual:', actual_counts
+
+
+def dict_eq(dict1, dict2, exclude=[]):
+    dict1_keys = set(dict1.keys()) - set(exclude)
+    dict2_keys = set(dict2.keys()) - set(exclude)
+
+    msg = "passed dictionary keys did not match: %s %s" % (
+            dict1_keys - dict2_keys, dict2_keys - dict1_keys)
+    eq_(dict1_keys, dict2_keys, msg)
+
+    for key in dict1_keys:
+        if isinstance(dict1[key], db.Model):
+            model_key_eq(dict1[key], dict2[key])
+        else:
+            eq_(dict1[key], dict2[key])
+
+
+def model_key_eq(model1, model2):
+    eq_(model1.key(), model2.key())
+
+
+def model_to_dict(model, exclude=[]):
+    model_dict = {}
+
+    for key, prop in model.properties().iteritems():
+        if key in exclude:
+            continue
+        model_dict[key] = getattr(model, key)
+
+    return model_dict

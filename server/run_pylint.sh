6c7f19e233a48259ce4d947af614e1694bc4c0a2
Nafis Jamal <nafisjamal@mopub.com>
2012-05-16 13:59:01 -0700

    updated pylint run

--- server/run_pylint.sh
+++ server/run_pylint.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 pushd ..
-pylint --rcfile=".pylintrc" --init-hook="import sys; sys.path.append('$VIRTUAL_ENV/lib/python2.7/site-packages'); sys.path.append('$PWD/optimizer')" -f parseable --reports=no `find . -name "*.py"` 2>&1 > pylint.txt
+touch server/__init__.py
+pylint --rcfile=".pylintrc" --init-hook="import sys; sys.path.append('$VIRTUAL_ENV/lib/python2.7/site-packages'); sys.path.append('$PWD/optimizer')" -f parseable --reports=no --ignore=common,boto,mapreduce,djangoflash,reporting,aws_reporting,appengine_console.py,migrater.py,appengine_config.py,ad_server,admin/beatbox,scripts,reports/aws_reports,account,appengine_django server
+rm -rf server/__init__.py*
 popd
